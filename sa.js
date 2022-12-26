4a753daa72ff50bf553307e12ad72893ad526c1d
owo <aburachenko2000@gmail.com>
2022-12-26 09:14:09 +0300

    arabic

---
+++ sa.js
@@ -0,0 +1,355 @@
+export default {
+  hello: 'asd',
+
+  title: {
+    home: 'الصفحة الرئيسية',
+    guest: 'تسجيل الدخول',
+    lists: {
+      0: 'قائمات',
+    },
+    tags: {
+      0: 'وسوم',
+      1: '{amount} الوسوم من أجل الفئة {name}',
+    },
+    ideas: {
+      0: 'إلهام',
+      1: '«{amount}»',
+    },
+    sounds: {
+      0: 'أصوات',
+    },
+    leaderboard: {
+      0: 'المتصدرون',
+    },
+    extend: {
+      0: 'أكثر | أقل',
+    },
+
+    skin: {
+      0: 'هذا الزي يحتوي على عناصر من أزياء أخرى',
+      1: 'اضغط هنا للمزيد من المعلومات',
+    },
+
+  },
+
+
+  changelogs: {
+    0: 'قائمة التغييرات',
+    1: 'قائمة جميع المراجعات المفعلة في الإصدار الحالي',
+    2: 'لا شيء هنا',
+    3: 'لكن بإمكانك إضافة التغييرات المجرأة على هذا الإصدار من الزي',
+    4: 'غير مجمعة',
+    5: 'جديد',
+    6: 'إصلاحات',
+    7: 'تغييرات',
+    8: 'حذف',
+  },
+
+  creators: {
+    0: 'المبتكرون',
+    1: 'قائمة جميع المبتكرين الذين شاركو في إنشاء هذا الإصدار',
+    2: 'لا احد هنا',
+    3: 'كتابة اسم لاعب سيريك نتائج البحث عنه في اللعبة',
+    4: 'محرر',
+    5: 'أدوار',
+    6: 'إصدارات',
+  },
+
+  tags: {
+    0: 'وسوم',
+    1: 'قائمة جميع الوسوم المتعلقة بهذا الزي',
+    2: 'يبدو هذا المكان فارغ',
+    3: 'استعمل الحقل أعلاه لإيجاد وسوم جديدة',
+  },
+
+  videos: {
+    0: 'مقاطع',
+    1: 'قائمة جميع المقاطع العارضة لهذا الزي',
+    2: 'عدم وجود عروض',
+    3: 'ابحث عنها في اليوتوب أو اصنع عرضك الخاص',
+  },
+
+  sources: {
+    0: 'إسناد',
+    1: 'قائمة جميع الأصول أو الأدوات المستعملة في هذا الاصدار من الزي',
+    2: 'المكان فارغ بعض الشيء',
+    3: 'إذا استعملت أصول أم فنون من أزياء أخرى، تأكد من إضافتها في العلبة أعلاه',
+    4: 'غير مجموعة',
+  },
+
+
+  checks: {
+    0: 'الكل',
+    1: 'بيانات فوقية',
+    2: 'إبراز',
+
+    4: 'ضف على الأقل مبتكر زي واحد',
+    5: 'ضف بعض الوسوم',
+    6: 'حجم الزي غير محدد',
+    7: 'اختر على الأقل نمط لعب واحد',
+    8: 'اختر نوع الملف (جودة عالية أو عادية)',
+    9: 'اختر على الأقل نسبة باعية واحدة',
+    10: 'عدم تقديم صور',
+    11: 'ضف على الأقل صورة واحدة لللعب',
+    12: 'عدم توفيير رابط للزي',
+
+    20: 'يبدو جيد',
+    21: 'تم إصلاح جميع المشاكل، و هذا جيد',
+    22: 'قائمة المراجعة',
+  },
+
+  skins: {
+    0: 'أزياء',
+    1: 'محبوبة',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'حديثة',
+    5: 'عشوائية',
+    6: 'جميع اللأزياء',
+    7: 'نسبة باعية',
+    8: 'جميع الأزياء | أزياء اللعبة',
+    9: 'زي | أزياء | أزياء',
+    10: 'لأزياء اللعبة من إصدار {name} | أزياء اللعبة من',
+    11: 'بحث',
+  },
+
+  skin: {
+    0: 'تعديل',
+    1: 'ميزات إضافية',
+    2: 'HD جودة عالية',
+    3: 'FullHD جودة أعلى',
+    4: 'متحرك',
+    5: 'صور',
+    6: 'لعب فقط',
+    7: 'رابط التحميل | تحميل الزي',
+
+    8: 'منشور المنتدى | منتدى اللعبة',
+    9: 'Reddit منشور',
+
+    10: 'مبتكرون',
+    11: 'وسوم',
+    13: 'مقاطع',
+
+    14: 'الإصدارات الجديدة | التحكم بالإصدارات',
+    15: 'تحديث | إرسال',
+
+    16: 'أنماط اللعب',
+    17: 'اسم الزي',
+    18: 'الإصدار | الإصدارات',
+    19: 'الحجم',
+    20: 'بيانات فوقية',
+
+    21: 'التقييم',
+    22: 'التحميلات',
+    23: 'سيتوفر التحميل قريبا',
+    24: '{amount} ثواني متبقية',
+  },
+
+  drag: {
+    0: 'إضغط هنا',
+    1: 'جر و اسقط الصور هنا',
+  },
+
+  footer: {
+    0: 'cyperdark صناعة',
+    1: 'اعتمادات',
+  },
+
+  credits: {
+    0: 'شخصيات كرتونية',
+    1: 'فنون',
+    2: 'أيقونات',
+    3: '{name} كل الأيقونات مأخوذة من',
+    4: 'أعلام',
+    5: '{name} كل الأعلام مأخوذة من'
+  },
+
+  search: {
+    0: 'البحث عن الزي',
+    1: 'مشاهدة',
+    2: 'عنوان',
+    3: 'تقييم',
+    4: 'حجم',
+    5: 'تاريخ',
+    7: 'لا نتائج',
+    8: 'حاول اختيار معيار بحث مختلف',
+    9: 'تتم معالجة الاستفسار',
+    10: 'اختر معيار البحث أو اكتب فقط عنوان الزي، المبتكر أو الوسم',
+  },
+
+  tabs: {
+    0: 'البحث',
+    1: 'تعديل',
+    2: 'قائمة',
+  },
+
+  undefined: {
+    0: 'فارغ',
+    1: 'لسوء الحظ لا يوجد شيء هنا، لكن بإمكانك النظر في صفحات أخرى',
+    2: 'لسوء الحظ لا يوجد شيء هنا',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  meta: {
+    title: {
+      0: 'Home page',
+      1: 'Gamemodes, aspect ratios osu skins',
+      2: 'Popular osu skins',
+      3: 'Search osu skins',
+      4: '«{name}» tags list',
+      5: '{name} v{version} osu skin',
+      6: 'Credits',
+      7: 'osu skins',
+      8: '{name} search osu skins',
+      9: 'Inspire yourself from other osu skins',
+      10: '{name} created osu skins',
+    },
+    description: {
+      // 0: 'Get the best osu skins at our site, with a huge library to choose from! We have skins for different gamemodes and aspect ratios, and even feature top players and anime themes. Boost your gameplay with our high-quality skins.',
+      0: 'Get the best osu! skins at our site! Our huge library has skins for all gamemodes, aspect ratios, and even top players. Plus, we have a special section for anime fans. Improve your gameplay with the perfect skin. #osuskins #osugame #anime',
+      1: '{views} views. An osu skin for {modes}, his weight is {size}, created by {creators}. Supports {ratios}',
+      2: 'Find the perfect osu! skin with our advanced search feature. Browse through our huge library, filtered by gamemode, aspect ratio, and more. Or, check out skins used by top players',
+      3: 'Discover a variety of game modes and aspect ratios on our site. Choose from standard mode, catch the beat, mania, and taiko. Find the perfect fit for your gaming preferences.',
+      4: 'Find the perfect osu skin from top players on our site. Browse our collection of popular skins and customize your gameplay. Discover a wide range of styles and themes to suit your preferences.',
+      5: 'List of existed tags for osu skins.',
+      6: '{name} osu skins collection!, free downloading',
+      7: 'A huge amount of osu skins for a {name}, download for free!',
+      8: 'All osu skins created by {name}',
+      9: 'Find inspiration for your next osu! skin at our site. Browse through a diverse collection of skins and discover new techniques and ideas. Get inspired and create your own unique skins.',
+    },
+    keywords: {
+      0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins'
+    },
+  },
+
+  buttons: {
+    profile_osu: 'osu! Profile',
+    new_skin: 'New skin',
+    notifications: 'Notifications',
+    settings: 'Settings',
+    logout: 'Logout',
+    load_more: 'Load more',
+    copy_short: 'Copy short link',
+    copied: 'Copied',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! Forum',
+    skin_return: 'Back to skin',
+  },
+
+
+
+  user: {
+    roles: {
+      0: 'Creator',
+      1: 'Mixer',
+      2: 'Animations',
+      3: 'Arts',
+      4: 'Collab',
+    }
+  },
+
+  tooltips: {
+    0: '{amount} views',
+    1: 'Refresh results',
+    2: 'Descending | Ascending',
+    3: '{amount} likes',
+    4: 'Press ctrl+enter to add a tag',
+    5: 'Edit version',
+    6: '{s}Log in{p}, to be able to rate this skin',
+    7: 'No short description',
+    8: 'No url',
+  },
+
+  lists: {
+    screens: {
+      0: 'Welcome',
+      1: 'Main menu',
+      2: 'Song select',
+      3: 'Mode menu',
+      4: 'Mods',
+      5: 'Countdown',
+      6: 'Gameplay',
+      7: 'Spinner',
+      8: 'Section Pass',
+      9: 'Section Fail',
+      10: 'Pause Screen',
+      11: 'Fail Screen',
+      12: 'Result screen',
+      13: 'Skip',
+      14: 'Banner',
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed',
+    }
+  },
+
+
+
+
+
+
+
+
+  select: {
+    0: 'New',
+    1: 'Fixed',
+    2: 'Changed',
+    3: 'Removed',
+
+    4: 'Art',
+    5: 'Font',
+    6: 'Artist',
+    7: 'Skin element',
+    8: 'Audio/Sfx',
+    9: 'Tools',
+
+
+
+    views: 'Views',
+    likes: 'Likes',
+    size_range: 'Select size',
+    date_range: 'Select a date range',
+    ratio: 'Select aspect ratio | ratio',
+  },
+
+  placeholders: {
+    0: 'Choose roles',
+    1: 'Choose versions',
+    2: 'Type text...',
+    3: 'Select size',
+    4: 'Select date range',
+    5: 'Year',
+    6: 'Month',
+    7: 'Select aspect ratio',
+    8: 'Select categories',
+    9: 'Gamemodes',
+    10: 'Select type',
+    11: 'Paste a youtube link',
+    12: 'Type description and url. Format description:url',
+    13: 'Select extra',
+    14: 'Paste a link',
+    15: 'Skin title, author, or tag',
+  }
+};
