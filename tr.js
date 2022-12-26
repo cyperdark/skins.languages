54a2f33792e4956f57eac1108a0d77a8c9ad886d
owo <aburachenko2000@gmail.com>
2022-12-26 04:33:39 +0300

    Turkey

---
+++ tr.js
@@ -0,0 +1,355 @@
+export default {
+  hello: 'asd',
+
+  title: {
+    home: 'Ana Sayfa',
+    guest: 'Giriş Yapın',
+    lists: {
+      0: 'Lists',
+    },
+    tags: {
+      0: 'Etiketler',
+      1: '{name} kategorisi için {amount} sonuç bulundu',
+    },
+    ideas: {
+      0: 'Keşfedin',
+      1: '«{amount}»',
+    },
+    sounds: {
+      0: 'Sesler',
+    },
+    leaderboard: {
+      0: 'Leaderboards',
+    },
+    extend: {
+      0: 'Çoktan aza | Azdan çoğa',
+    },
+
+    skin: {
+      0: 'Bu skin içinde diğer skinlerden elemntler barındırıyor',
+      1: 'Daha fazla bilgi için buraya tıklayın',
+    },
+
+  },
+
+
+  changelogs: {
+    0: 'Changelog',
+    1: 'Güncel versiyondaki yapılan tüm düzeltmelerin listesi',
+    2: 'Burada hiçbir şey yok',
+    3: 'Ama skinin bu versiyonuna yaptığınız değişiklikleri ekleyebilirsiniz',
+    4: 'Gruplandırılmamış',
+    5: 'Yeni',
+    6: 'Düzeltmeler',
+    7: 'Değişiklikler',
+    8: 'Silinenler',
+  },
+
+  creators: {
+    0: 'Oluşturan',
+    1: 'Bu versiyonu oluşturmakta emeği geçenlerin listesi',
+    2: 'Burada kimse yok',
+    3: 'Typing in a player\'s name will show you the results of the osu search.',
+    4: 'Editor',
+    5: 'Roller',
+    6: 'Versions',
+  },
+
+  tags: {
+    0: 'Etiketler',
+    1: 'Bu skinle bağlantılı tüm etiketlerin listesi',
+    2: 'Burası baya bi boş gözüküyor.',
+    3: 'Yeni etiketler bulmak için yukarıdaki alanı kullanın',
+  },
+
+  videos: {
+    0: 'Videos',
+    1: 'Bu skin için sergilenen tüm videoların listesi',
+    2: 'Zero showcases',
+    3: 'Youtubedan alın ya da kendinizinkini oluşturun',
+  },
+
+  sources: {
+    0: 'Özellik',
+    1: 'Skinde kullanılan tüm assetlerin ve toolların listesi',
+    2: 'Kinda empty here',
+    3: 'Eğer diğer skinlerden asset ya da resim kullandıysanız, yukarıdaki kutucuğa eklediğinizden emin olun',
+    4: 'Gruplandırılmamış',
+  },
+
+
+  checks: {
+    0: 'All',
+    1: 'Metadata',
+    2: 'Hightlight',
+
+    4: 'En az bir oluşturan ekleyin',
+    5: 'Birkaç etiket ekleyin',
+    6: 'Skinin boyutu belirtilmemiş',
+    7: 'En az bir oyun modu seçin',
+    8: 'Dosya türü seçin (SD ya da HD)',
+    9: 'En az bir ekran oranı seçin',
+    10: 'No screenshots provided',
+    11: 'En az bir oynanış görüntüsü ekleyin',
+    12: 'Skin linki belirtilmemiş',
+
+    20: 'Looking pretty good',
+    21: 'İyi haber! Hiçbir problem kalmadı',
+    22: 'Check list',
+  },
+
+  skins: {
+    0: 'Skinler',
+    1: 'Popüler',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'En yeni',
+    5: 'Rastgele',
+    6: 'Tüm skinler',
+    7: 'Çerçeve Oranı',
+    8: 'All skins | osu skins',
+    9: 'Skin | Skins | Skins',
+    10: '{name} tarafından yapıldı | All skins from',
+    11: 'Search',
+  },
+
+  skin: {
+    0: 'Edit',
+    1: 'Extra',
+    2: 'HD',
+    3: 'FullHD',
+    4: 'Animasyon',
+    5: 'Screenshots',
+    6: 'Sadece Gameplay',
+    7: 'İndirme linki | skini indir',
+
+    8: 'Forum post |osu! Forum',
+    9: 'Reddit post',
+
+    10: 'Creators',
+    11: 'Etiketler',
+    13: 'Videos',
+
+    14: 'New Version | Manage versions',
+    15: 'Güncelle | Submit',
+
+    16: 'Gamemodes',
+    17: 'Skinin adı',
+    18: 'Version | Versions',
+    19: 'Boyut',
+    20: 'Metadata',
+
+    21: 'Rating',
+    22: 'İndirilenler',
+    23: 'En kısa zamanda indirmeye açılacak',
+    24: '{amount} saniye kaldı',
+  },
+
+  drag: {
+    0: 'Buraya tıklayın',
+    1: 'Resimleri buraya sürükle ya da bırak',
+  },
+
+  footer: {
+    0: 'cyperdark tarafından yapıldı',
+    1: 'Credits',
+  },
+
+  credits: {
+    0: 'Chibi',
+    1: 'Resimler',
+    2: 'Icons',
+    3: 'All icons are taken from {name}',
+    4: 'Flags',
+    5: 'All flag images are from {name}'
+  },
+
+  search: {
+    0: 'Skin aratın',
+    1: 'Görüntülenme',
+    2: 'Başlık',
+    3: 'Rating',
+    4: 'Boyut',
+    5: 'Tarih',
+    7: 'Hiçbir şey bulunamadı',
+    8: 'Başka kriterde arama yapmayı deneyin',
+    9: 'Arama bekleniyor',
+    10: 'Arama kriterinizi seçin ya da sadece skin başlığını, yapımcısını, ya da etiketini yazın',
+  },
+
+  tabs: {
+    0: 'Arama',
+    1: 'Edit',
+    2: 'List',
+  },
+
+  undefined: {
+    0: 'Boş',
+    1: 'Ne yazık ki burada hiçbir şey yok, diğer sayfalara göz atabilirsin',
+    2: 'Ne yazık ki burada hiçbir şey yok',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  meta: {
+    title: {
+      0: 'Ana sayfa',
+      1: 'Gamemodes, aspect ratios osu skins',
+      2: 'Populer osu skinleri',
+      3: 'Search osu skins',
+      4: '«{name}» tags list',
+      5: '{name} v{version} osu skin',
+      6: 'Credits',
+      7: 'osu skins',
+      8: '{name} search osu skins',
+      9: 'Diğer osu skinlerinden ilham alın',
+      10: '{name} created osu skins',
+    },
+    description: {
+      // 0: 'Get the best osu skins at our site, with a huge library to choose from! We have skins for different gamemodes and aspect ratios, and even feature top players and anime themes. Boost your gameplay with our high-quality skins.',
+      0: 'Get the best osu! skins at our site! Our huge library has skins for all gamemodes, aspect ratios, and even top players. Plus, we have a special section for anime fans. Improve your gameplay with the perfect skin. #osuskins #osugame #anime',
+      1: '{views} views. An osu skin for {modes}, his weight is {size}, created by {creators}. Supports {ratios}',
+      2: 'Find the perfect osu! skin with our advanced search feature. Browse through our huge library, filtered by gamemode, aspect ratio, and more. Or, check out skins used by top players',
+      3: 'Discover a variety of game modes and aspect ratios on our site. Choose from standard mode, catch the beat, mania, and taiko. Find the perfect fit for your gaming preferences.',
+      4: 'Find the perfect osu skin from top players on our site. Browse our collection of popular skins and customize your gameplay. Discover a wide range of styles and themes to suit your preferences.',
+      5: 'List of existed tags for osu skins.',
+      6: '{name} osu skins collection!, free downloading',
+      7: 'A huge amount of osu skins for a {name}, download for free!',
+      8: 'All osu skins created by {name}',
+      9: 'Find inspiration for your next osu! skin at our site. Browse through a diverse collection of skins and discover new techniques and ideas. Get inspired and create your own unique skins.',
+    },
+    keywords: {
+      0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins'
+    },
+  },
+
+  buttons: {
+    profile_osu: 'osu! Profile',
+    new_skin: 'Yeni skin',
+    notifications: 'Bildirimler',
+    settings: 'Ayarlar',
+    logout: 'Çıkış yapın',
+    load_more: 'Daha fazla yükle',
+    copy_short: 'Linki kopyala',
+    copied: 'Kopyalandı!',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! Forum',
+    skin_return: 'Skine dön',
+  },
+
+
+
+  user: {
+    roles: {
+      0: 'Oluşturan',
+      1: 'Mixer',
+      2: 'Animasyonlar',
+      3: 'Resimler',
+      4: 'Collab',
+    }
+  },
+
+  tooltips: {
+    0: '{amount} Görüntülenme',
+    1: 'Sonuçları yeniden yükle',
+    2: 'Descending | Ascending',
+    3: '{amount} Beğeni',
+    4: 'Etiket eklemek için ctrl+enter basın',
+    5: 'Edit version',
+    6: '{s}Giriş yapın{p}, Bu skini değerlendirmek için',
+    7: 'No short description',
+    8: 'No url',
+  },
+
+  lists: {
+    screens: {
+      0: 'Welcome',
+      1: 'Ane menü',
+      2: 'Song select',
+      3: 'Mode menu',
+      4: 'Modlar',
+      5: 'Countdown',
+      6: 'Gameplay',
+      7: 'Spinner',
+      8: 'Section Pass',
+      9: 'Section Fail',
+      10: 'Durdurma Ekranı',
+      11: 'Fail Ekranı',
+      12: 'Result screen',
+      13: 'Skip',
+      14: 'Banner',
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed',
+    }
+  },
+
+
+
+
+
+
+
+
+  select: {
+    0: 'Yeni',
+    1: 'Düzeltildi',
+    2: 'Değiştirildi',
+    3: 'Silindi',
+
+    4: 'Art',
+    5: 'Font',
+    6: 'Artist',
+    7: 'Skin element',
+    8: 'Audio/Sfx',
+    9: 'Tools',
+
+
+
+    views: 'Görüntülenme',
+    likes: 'Beğeni',
+    size_range: 'Boyut seçin',
+    date_range: 'Tarih aralığı seçin',
+    ratio: 'Ekran oranı seçin | ratio',
+  },
+
+  placeholders: {
+    0: 'Rol seçin',
+    1: 'Versiyon seçin',
+    2: 'Type text...',
+    3: 'Boyut seçin',
+    4: 'Tarih aralığı seçin',
+    5: 'Yıl',
+    6: 'Ay',
+    7: 'Ekran oranı seçin',
+    8: 'Kategorileri seçin',
+    9: 'Oyun modları',
+    10: 'Tür seçin',
+    11: 'youtube bağlantısını yapıştırın',
+    12: 'Açıklama ve url. yazın Format description:url',
+    13: 'Select extra',
+    14: 'Paste a link',
+    15: 'Skin başlığı, yapımcısı, ya da etiket',
+  }
+};
