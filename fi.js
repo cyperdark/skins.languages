631cd51755288cbbaaae8a8ddbdc506b81c2b941
owo <aburachenko2000@gmail.com>
2022-12-26 11:51:40 +0300

    New translations en.js (Finnish)

--- fi.js
+++ fi.js
@@ -1,38 +1,26 @@
 export default {
   hello: 'asd',
-
   title: {
     home: 'Kotisivu',
+    page: 'page',
     guest: 'Kirjaudu Sisään',
-    lists: {
-      0: 'Luettelot',
-    },
+    lists: { 0: 'Luettelot' },
     tags: {
       0: 'Tägit',
-      1: '{amount} tägiä per kategoria {name}',
+      1: '{amount} tägiä per kategoria {name}'
     },
     ideas: {
       0: 'Inspiraatio',
-      1: '«{amount}»',
+      1: '«{amount}»'
     },
-    sounds: {
-      0: 'Äänet',
-    },
-    leaderboard: {
-      0: 'Tulostaulut',
-    },
-    extend: {
-      0: 'Enemmän | Vähemmän',
-    },
-
+    sounds: { 0: 'Äänet' },
+    leaderboard: { 0: 'Tulostaulut' },
+    extend: { 0: 'Enemmän | Vähemmän' },
     skin: {
       0: 'Tässä skinissä on käytetty tiedostoja muista skineistä',
-      1: 'Klikkaa tästä saadaksesi lisää tietoa',
-    },
-
+      1: 'Klikkaa tästä saadaksesi lisää tietoa'
+    }
   },
-
-
   changelogs: {
     0: 'Muutosloki',
     1: 'Luettelo kaikista nykyiseen versioon tehdyistä muutoksista',
@@ -42,9 +30,8 @@ export default {
     5: 'Uutta',
     6: 'Korjaukset',
     7: 'Muutokset',
-    8: 'Poistot',
+    8: 'Poistot'
   },
-
   creators: {
     0: 'Tekijät',
     1: 'Luettelo kaikista tekijöistä jotka olivat mukana tämän version tekemisessä',
@@ -52,37 +39,31 @@ export default {
     3: 'Kirjoittamalla pelaajan nimen näet osu haun tulokset.',
     4: 'Muokkaaja',
     5: 'Roolit',
-    6: 'Versiot',
+    6: 'Versiot'
   },
-
   tags: {
     0: 'Tägit',
     1: 'Luettelo kaikista tägeistä jotka liittyvät tähän skiniin',
     2: 'Täällä tuntuu tyhjältä.',
-    3: 'Käytä yllä olevaa kenttää uusien tägien löytämiseen',
+    3: 'Käytä yllä olevaa kenttää uusien tägien löytämiseen'
   },
-
   videos: {
     0: 'Videot',
     1: 'Luettelo kaikista tämän skinin videoesittelyistä',
     2: 'Ei esittelyjä',
-    3: 'Voit etsiä niitä YouTubesta tai tehdä oman esittelysi',
+    3: 'Voit etsiä niitä YouTubesta tai tehdä oman esittelysi'
   },
-
   sources: {
     0: 'Attribution',
     1: 'Luettelo käytetyistä muiden skinejen omaisuuksista tai työkaluista tässä versiossa',
     2: 'Vähän tyhjää täällä',
     3: 'Jos olet käyttänyt muiden skinien omaisuuksia tai taidetta, muista lisätä ne ylläolevaan laatikkoon',
-    4: 'Ei ryhmitetty',
+    4: 'Ei ryhmitelty'
   },
-
-
   checks: {
     0: 'Kaikki',
     1: 'Metadata',
     2: 'Korosta',
-
     4: 'Lisää ainakin yksi skinin tekijä',
     5: 'Lisää muutama tägi',
     6: 'Skinin koko ei ole määritelty',
@@ -92,12 +73,10 @@ export default {
     10: 'Ei annettuja kuvakaappauksia',
     11: 'Lisää ainakin yksi pelialueen kuvakaappaus',
     12: 'Skinin linkki ei ole määritelty',
-
     20: 'Näyttää aika hyvältä',
     21: 'Jokainen ongelma on korjattu, hyvää työtä',
-    22: 'Tarkistuslista',
+    22: 'Tarkistuslista'
   },
-
   skins: {
     0: 'Skinit',
     1: 'Suositut',
@@ -110,9 +89,8 @@ export default {
     8: 'Kaikki skinit | osu skinit',
     9: 'Skini | Skinit | Skinit',
     10: 'osu Skinit joissa {name} on tekijä | Kaikki skinit joiden tekijä on',
-    11: 'Haku',
+    11: 'Haku'
   },
-
   skin: {
     0: 'Muuta',
     1: 'Ekstra',
@@ -122,39 +100,31 @@ export default {
     5: 'Kuvakaappaukset',
     6: 'Vain pelialue',
     7: 'Lataa linkki | Lataa skini',
-
     8: 'Foorumi päivitys |osu! Foorumi',
     9: 'Reddit päivitys',
-
     10: 'Tekijät',
     11: 'Tägit',
     13: 'Videot',
-
     14: 'Uusi Versio | Hallitse Versioita',
     15: 'Päivitä | Lähetä',
-
     16: 'Pelimodet',
     17: 'Skinin Nimi',
     18: 'Versio | Versiot',
     19: 'Koko',
     20: 'Metadata',
-
     21: 'Arvosana',
     22: 'Lataukset',
     23: 'Lataus on valmiina pian',
-    24: '{amount} sekunteja jäljellä',
+    24: '{amount} sekunteja jäljellä'
   },
-
   drag: {
     0: 'Paina tästä',
-    1: 'tai vedä ja pudota kuvia',
+    1: 'tai vedä ja pudota kuvia'
   },
-
   footer: {
     0: 'Tekijä: cyperdark',
-    1: 'Kiitokset',
+    1: 'Kiitokset'
   },
-
   credits: {
     0: 'Chibi',
     1: 'Taiteet',
@@ -163,7 +133,6 @@ export default {
     4: 'Liput',
     5: 'Kaikki lippukuvakkeet ovat lähteestä {name}'
   },
-
   search: {
     0: 'Skini haku',
     1: 'Näyttökerrat',
@@ -174,42 +143,18 @@ export default {
     7: 'Mitään ei löytynyt',
     8: 'Yritä erilaisia hakuasetuksia',
     9: 'Odotetaan hakua',
-    10: 'Valitse hakusuodattimet tai kirjoita skinin nimi, tekijä tai tägi',
+    10: 'Valitse hakusuodattimet tai kirjoita skinin nimi, tekijä tai tägi'
   },
-
   tabs: {
     0: 'Haku',
-    1: 'Muokkaa',
-    2: 'Luettelo',
+    1: 'Muuta',
+    2: 'Luettelo'
   },
-
   undefined: {
     0: 'Tyhjä',
     1: 'Valitettavasti täällä ei ole mitään, voit etsiä eri sivuja',
-    2: 'Valitettavasti täällä ei ole mitään',
+    2: 'Valitettavasti täällä ei ole mitään'
   },
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   meta: {
     title: {
       0: 'Kotisivu',
@@ -222,10 +167,10 @@ export default {
       7: 'osu skinit',
       8: '{name} hae osu skinejä',
       9: 'Inspiroi itseäsi muista osu skineistä',
-      10: '{name} tehtyjä osu skinejä',
+      10: '{name} tehtyjä osu skinejä'
     },
     description: {
-      // 0: 'Hanki parhaat osu skinit sivumme suuresta valikoimasta! Meillä on skinejä eri pelimodeille ja kuvasuhteille, sekä jopa teemoja animesta ja top pelaajista. Paranna peliäsi meidän korkealaatuisilla skineillä.',
+      // 0: 'Get the best osu skins at our site, with a huge library to choose from! We have skins for different gamemodes and aspect ratios, and even feature top players and anime themes. Boost your gameplay with our high-quality skins.',
       0: 'Hanki parhaat osu skinit sivumme suuresta valikoimasta! Meillä on skinejä eri pelimodeille ja kuvasuhteille, sekä jopa top pelaajille. Lisäksi, meillä on erityinen osasto animefaneille. Paranna peliäsi täydellisellä skinillä. #osuskins #osugame #anime',
       1: '{views} näyttökertaa. {modes} osu skini, jonka koko on {size}, tekijänä on {creators}. Tukee {ratios}',
       2: 'Löydä täydellinen osu! skini meidän edistyneellä hakukoneella. Selaa meidän jättimäisen valikoiman läpi, joka on suodatettu pelimodella, kuvasuhteella, ja monella muulla. Tai, katso top pelaajien käyttämiä skinejä.',
@@ -235,13 +180,10 @@ export default {
       6: '{name} osu skini kokoelma!, ilmainen lataus.',
       7: 'Suuri määrä skinejä jotka {name} on tehnyt, lataa ilmaiseksi!',
       8: 'Kaikki skinit jotka {name} on tehnyt',
-      9: 'Löydä inspiraatiota seuraavalle osu skinillesi nettisivultamme. Selaa meidän monipuolisen skini kokoelman läpi ja löydä uusia tapoja ja ideoita. Inspiroidu ja tee oma ainutlaatuinen skinisi.',
+      9: 'Löydä inspiraatiota seuraavalle osu skinillesi nettisivultamme. Selaa meidän monipuolisen skini kokoelman läpi ja löydä uusia tapoja ja ideoita. Inspiroidu ja tee oma ainutlaatuinen skinisi.'
     },
-    keywords: {
-      0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins'
-    },
+    keywords: { 0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins' }
   },
-
   buttons: {
     profile_osu: 'osu! Profiili',
     new_skin: 'Uusi skini',
@@ -253,21 +195,17 @@ export default {
     copied: 'Kopioitu',
     reddit_link: 'Reddit',
     osu_forum_link: 'osu! Foorumi',
-    skin_return: 'Takaisin skiniin',
+    skin_return: 'Takaisin skiniin'
   },
-
-
-
   user: {
     roles: {
       0: 'Tekijä',
       1: 'Miksaaja',
       2: 'Animaatiot',
       3: 'Taiteet',
-      4: 'Yhteistyö',
+      4: 'Yhteistyö'
     }
   },
-
   tooltips: {
     0: '{amount} näyttökertaa',
     1: 'Päivitä tulokset',
@@ -277,9 +215,8 @@ export default {
     5: 'Muokkaa versio',
     6: '{s}Kirjaudu sisään{p}, jotta voit arvostella tämän skinin',
     7: 'Ei lyhyttä kuvausta',
-    8: 'Ei url-osoitetta',
+    8: 'Ei url-osoitetta'
   },
-
   lists: {
     screens: {
       0: 'Tervetuloa',
@@ -296,44 +233,31 @@ export default {
       11: 'Epäonnistumisnäyttö',
       12: 'Tulosnäyttö',
       13: 'Ohitus',
-      14: 'Banneri',
+      14: 'Banneri'
     },
     cats: {
       0: 'NSFW',
       1: 'Turri',
-      2: 'Sekoitus',
+      2: 'Sekoitus'
     }
   },
-
-
-
-
-
-
-
-
   select: {
-    0: 'Uusi',
+    0: 'Uutta',
     1: 'Korjattu',
     2: 'Muutettu',
     3: 'Poistettu',
-
     4: 'Taide',
     5: 'Fontti',
     6: 'Artisti',
     7: 'Skinin tiedosto',
     8: 'Ääni/Sfx',
     9: 'Työkalut',
-
-
-
     views: 'Näyttökerrat',
     likes: 'Tykkäykset',
     size_range: 'Valitse koko',
     date_range: 'Valitse ajanjakso',
-    ratio: 'Valitse kuvasuhde | suhde',
+    ratio: 'Valitse kuvasuhde | suhde'
   },
-
   placeholders: {
     0: 'Valitse roolit',
     1: 'Valitse versiot',
@@ -350,6 +274,6 @@ export default {
     12: 'Kirjoita kuvaus ja url formaattina: description:url',
     13: 'Valitse lisät',
     14: 'Liitä linkki',
-    15: 'Skinin otsikko, tekijä, tai tägi',
+    15: 'Skinin otsikko, tekijä, tai tägi'
   }
 };
