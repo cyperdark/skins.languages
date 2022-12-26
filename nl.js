846333beba4d65d3ac077c2c61f042b9a48934fc
owo <aburachenko2000@gmail.com>
2022-12-26 04:33:58 +0300

    Dutch

---
+++ nl.js
@@ -0,0 +1,355 @@
+export default {
+  hello: 'asd',
+
+  title: {
+    home: 'Thuispagina',
+    guest: 'Inloggen',
+    lists: {
+      0: 'Lijsten',
+    },
+    tags: {
+      0: 'Categorieën',
+      1: '{amount} tags voor categorie {name}',
+    },
+    ideas: {
+      0: 'Inspiratie',
+      1: '«{amount}»',
+    },
+    sounds: {
+      0: 'Geluiden',
+    },
+    leaderboard: {
+      0: 'Ranglijsten',
+    },
+    extend: {
+      0: 'Meer | Minder',
+    },
+
+    skin: {
+      0: 'Deze skin heeft een of meerdere elementen van andere skins',
+      1: 'Klik hier voor meer informatie',
+    },
+
+  },
+
+
+  changelogs: {
+    0: 'Veranderingen',
+    1: 'Lijst van alle gemaakte wijzigingen in de huidige versie',
+    2: 'Niks hier',
+    3: 'Maar je kan veranderingen toevoegen die jij hebt gemaakt bij deze versie van de skin',
+    4: 'Niet gegroepeerd',
+    5: 'Nieuw',
+    6: 'Verbeteringen',
+    7: 'Veranderingen',
+    8: 'Verwijderingen',
+  },
+
+  creators: {
+    0: 'Makers',
+    1: 'Lijst van alle makers die hielpen met het maken van deze versie',
+    2: 'Er is niemand is hier',
+    3: 'Het typen van de naam van een speler zal je de resultaten van het zoeken in osu laten zien.',
+    4: 'Editor',
+    5: 'Rollen',
+    6: 'Versies',
+  },
+
+  tags: {
+    0: 'Tags',
+    1: 'Lijst van alle tags gerelateerd aan deze skin',
+    2: 'Het is hier vrij leeg.',
+    3: 'Gebruik het bovenstaande veld om nieuwe tags te vinden',
+  },
+
+  videos: {
+    0: 'Video\'s',
+    1: 'Lijst van alle video\'s gerelateerd aan deze skin',
+    2: 'Nul showcases',
+    3: 'Zoek er een op YouTube of maak er zelf een',
+  },
+
+  sources: {
+    0: 'Attributen',
+    1: 'Lijst van alle gebruikte hulpmiddelen in de versie van de skin',
+    2: 'Beetje leeg hier',
+    3: 'Als je hulpmiddelen van andere skins of kunst hebt gebruikt, voeg ze vooral toe in het vak hierboven',
+    4: 'Niet gegroepeerd',
+  },
+
+
+  checks: {
+    0: 'Alles',
+    1: 'Metadata',
+    2: 'Uitgelicht',
+
+    4: 'Voeg tenminste één maker van de skin toe',
+    5: 'Voeg een paar tags toe',
+    6: 'Grootte van de skin is niet gegeven',
+    7: 'Selecteer tenminste één gamemode',
+    8: 'Selecteer een file type (SD of HD)',
+    9: 'Selecteer tenminste één aspect ratio',
+    10: 'Geen schermopnames aangeboden',
+    11: 'Voeg tenminste één schermopname toe terwijl je speelt',
+    12: 'Link naar de skin is niet gegeven',
+
+    20: 'Ziet er lekker uit',
+    21: 'Elk probleem is opgelost en dat is best fijn',
+    22: 'Checklist',
+  },
+
+  skins: {
+    0: 'Skins',
+    1: 'Populair',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'Recent',
+    5: 'Willekeurig',
+    6: 'Alle skins',
+    7: 'Aspect ratio',
+    8: 'Alle skins | osu skins',
+    9: 'Skin | Skins | Skins',
+    10: 'osu Skins op {name} | Alle skins van',
+    11: 'Zoek',
+  },
+
+  skin: {
+    0: 'Bewerk',
+    1: 'Extra',
+    2: 'HD',
+    3: 'FullHD',
+    4: 'Animatie',
+    5: 'Schermopnames',
+    6: 'Alleen van tijdens het spelen',
+    7: 'Download link | Download skin',
+
+    8: 'Forum post |osu! Forum',
+    9: 'Reddit post',
+
+    10: 'Maker',
+    11: 'Tags',
+    13: 'Video\'s',
+
+    14: 'Nieuwe Versie | Beheer versies',
+    15: 'Update | Lever in',
+
+    16: 'Gamemodes',
+    17: 'Naam van de skin',
+    18: 'Versie | Versies',
+    19: 'Grootte',
+    20: 'Metadata',
+
+    21: 'Beoordeling',
+    22: 'Downloads',
+    23: 'Download komt er binnenkort aan',
+    24: '{amount} seconden over',
+  },
+
+  drag: {
+    0: 'Klik hier',
+    1: 'of sleep foto\'s hierheen',
+  },
+
+  footer: {
+    0: 'Gemaakt door cyperdark',
+    1: 'Dank aan',
+  },
+
+  credits: {
+    0: 'Chibi',
+    1: 'Kunstwerken',
+    2: 'Icoontjes',
+    3: 'Alle icoontjes zijn genomen van {name}',
+    4: 'Vlaggen',
+    5: 'Alle vlaggen zijn genomen van {name}'
+  },
+
+  search: {
+    0: 'Zoek naar skin',
+    1: 'Weergaven',
+    2: 'Titel',
+    3: 'Beoordeling',
+    4: 'Grootte',
+    5: 'Datum',
+    7: 'Niks gevonden',
+    8: 'Probeer andere zoek criteria',
+    9: 'We wachten op je zoekopdracht',
+    10: 'Selecteer de zoek criteria of type gewoon de titel, maker of tag van de skin',
+  },
+
+  tabs: {
+    0: 'Zoek',
+    1: 'Bewerk',
+    2: 'Lijst',
+  },
+
+  undefined: {
+    0: 'Leeg',
+    1: 'Helaas pindakaas, er is hier niks, maar je kan op andere pagina\'s wel wat vinden',
+    2: 'Helaas pindakaas, er is hier niks',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  meta: {
+    title: {
+      0: 'Thuispagina',
+      1: 'Gamemodes, aspect ratio\'s van de skins',
+      2: 'Populaire osu skins',
+      3: 'Zoek osu skins',
+      4: '«{name}» tags lijst',
+      5: '{name} v{version} osu skin',
+      6: 'Dank aan',
+      7: 'osu skins',
+      8: '{name} zoek osu skins',
+      9: 'Inspireer jezelf door andere osu skins',
+      10: '{name} maakte osu skins',
+    },
+    description: {
+      // 0: 'Neem de beste osu! skins van onze website met een grote assortiment om uit te kiezen. We hebben skins voor alle gamemodes, aspect ratio’s  en er zijn zelfs skins van de beste spelers en anime thema’s. Speel beter met onze hoge skins van hoge kwaliteit.',
+      0: 'Neem de beste osu! skins van onze website! Onze grote bibliotheek heeft skins voor alle gamemodes, aspect ratio’s  en we hebben zelfs skins van de beste spelers. Maar dan heb je anime sectie nog niet gezien. Speel beter met onze hoge skins van hoge kwaliteit. #osuskins #osugame #anime',
+      1: '{views} weergaven. Een osu skin voor {modes}, de grootte is {size}, gemaakt door {creators}. Ondersteunt {ratios}',
+      2: 'Vind de perfecte osu! skin met onze geavanceerd zoek mechanisme. Verdwaal in onze gigantische bibliotheek, gefilterd op gamemode, aspect ratio en meer, of neem een kijkje bij skins die de beste spelers gebruiken.',
+      3: 'Ontdek een diverse hoeveelheid skins voor elke gamemode en aspect ratio op onze site. Kies uit standaard, catch the beat, mania of taiko en vindt wat het beste past bij jou.',
+      4: 'Vindt de perfecte osu skin van de beste spelers op onze site. Verdwaal in onze collectie van populaire skins en verander ze naar jouw voorkeuren. Ga op avontuur in een diverse selectie  van stijlen en thema’s om voor jouw de perfecte skin te vinden.',
+      5: 'Lijst van bestaande tags voor osu skins.',
+      6: '{name} osu skin collectie!, gratis om te downloaden',
+      7: 'Een gigantische hoeveelheid osu skins voor een {name}, gratis te downloaden!',
+      8: 'Alle osu skins gemaakt door {name}',
+      9: 'Vindt inspiratie voor jouw volgende osu! skin op onze site. Verdwaal in ons gevarieerde bos van skins en ontdek nieuwe technieken en ideeën. Wordt geïnspireerd en maak je eigen unieke skins.',
+    },
+    keywords: {
+      0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins'
+    },
+  },
+
+  buttons: {
+    profile_osu: 'osu! Profiel',
+    new_skin: 'Nieuwe skin',
+    notifications: 'Notificaties',
+    settings: 'Instellingen',
+    logout: 'Uitloggen',
+    load_more: 'Laad meer in',
+    copy_short: 'Kopieer de korte link',
+    copied: 'Gekopieerd',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! Forum',
+    skin_return: 'Terug naar skin',
+  },
+
+
+
+  user: {
+    roles: {
+      0: 'Maker',
+      1: 'Mixer',
+      2: 'Animaties',
+      3: 'Kunstwerken',
+      4: 'Samenwerking ',
+    }
+  },
+
+  tooltips: {
+    0: '{amount} weergaven',
+    1: 'Herlaad resultaten',
+    2: 'Hoog naar laag | Laag naar hoog',
+    3: '{amount} duimpies',
+    4: 'Druk op ctrl+enter om een tag toe te voegen',
+    5: 'Verander versie',
+    6: '{s}Log in{p}, om deze skin een rating te kunnen geven',
+    7: 'Geen korte beschrijving',
+    8: 'Geen url',
+  },
+
+  lists: {
+    screens: {
+      0: 'Welkom',
+      1: 'Hoofdmenu',
+      2: 'Nummer selecteren',
+      3: 'Mode menu',
+      4: 'Mods',
+      5: 'Aftelling',
+      6: 'Gameplay',
+      7: 'Spinner',
+      8: 'Sectie gehaald',
+      9: 'Sectie gefaald',
+      10: 'Pauzeerscherm',
+      11: 'Gefaaldscherm',
+      12: 'Resultatnscherm',
+      13: 'Sla over',
+      14: 'Banner',
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed',
+    }
+  },
+
+
+
+
+
+
+
+
+  select: {
+    0: 'Nieuw',
+    1: 'Verbeterd',
+    2: 'Veranderd',
+    3: 'Verwijderd',
+
+    4: 'Kunst',
+    5: 'Font',
+    6: 'Artiest',
+    7: 'Skin element',
+    8: 'Geluid/Geluidseffecten',
+    9: 'Hulpmiddelen',
+
+
+
+    views: 'Weergaven',
+    likes: 'Duimpies',
+    size_range: 'Selecteer grootte',
+    date_range: 'Selecteer een tijdsperiode',
+    ratio: 'Selecteer een aspect ratio | ratio',
+  },
+
+  placeholders: {
+    0: 'Kies rollen',
+    1: 'Kies versies',
+    2: 'Typ tekst...',
+    3: 'Selecteer grootte',
+    4: 'Selecteer tijdsperiode',
+    5: 'Jaar',
+    6: 'Maand',
+    7: 'Selecteer aspect ratio',
+    8: 'Selecteer categorieën',
+    9: 'Gamemodes',
+    10: 'Selecteer type',
+    11: 'Plak een youtube link',
+    12: 'Typ een omschrijving en URL. Formaat omschrijving:URL',
+    13: 'Selecteer extra',
+    14: 'Plak een link',
+    15: 'Skin titel, maker of tag',
+  }
+};
