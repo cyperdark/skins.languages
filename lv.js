eb11fb775a6790e9dfdbeb5069963e3c28f8f4c5
owo <aburachenko2000@gmail.com>
2022-12-27 02:49:14 +0300

    New translations en.js (Latvian)

---
+++ lv.js
@@ -0,0 +1,286 @@
+export default {
+  hello: 'asd',
+  title: {
+    home: 'Mājaslapa',
+    page: 'lapa',
+    guest: 'Pieslēgties',
+    lists: { 0: 'Saraksti' },
+    tags: {
+      0: 'Tagi',
+      1: '{amount} tagi kategorijai {name}'
+    },
+    ideas: {
+      0: 'Iedvesma',
+      1: '«{amount}»'
+    },
+    sounds: { 0: 'Skaņas' },
+    leaderboard: { 0: 'Līdersaraksti' },
+    extend: { 0: 'Vairāk | Mazāk' },
+    skin: {
+      0: 'Šis skins satur elementus no citiem skiniem',
+      1: 'Spied šeit, lai iegūtu vairāk informācijas'
+    }
+  },
+  changelogs: {
+    0: 'Izmaiņu saraksts',
+    1: 'Saraksts ar visām veiktajām revīzijām pašreizējā versijā',
+    2: 'Šeit nekā nav',
+    3: 'Bet tu vari pievienot izmaiņas, kuras tu veici šai skina versijai',
+    4: 'Nav grupēts',
+    5: 'Jaunais',
+    6: 'Labotais',
+    7: 'Pārmainītais',
+    8: 'Izņemtais'
+  },
+  creators: {
+    0: 'Veidotāji',
+    1: 'Kreditē visus, kuri bija iesaistīti šīs versijas izveidē',
+    2: 'Neviena šeit nav',
+    3: 'Ierakstot spēlētāja vārdu, tev parādīsies osu meklēšanas rezultāti.',
+    4: 'Rediģētājs',
+    5: 'Lomas',
+    6: 'Versijas'
+  },
+  tags: {
+    0: 'Tagi',
+    1: 'Saraksts ar visiem tagiem, kas ir saistīti ar šo skinu',
+    2: 'Šeit liekas diezgan klusi.',
+    3: 'Izmanto augšējo lauku, lai atrastu jaunus tagus'
+  },
+  videos: {
+    0: 'Video',
+    1: 'Saraksts ar visām video izstādēm šim skinam',
+    2: 'Nulle izrāžu',
+    3: 'Meklē YouTubē vai uztaisi pats savu izrādi'
+  },
+  sources: {
+    0: 'Atribūcija',
+    1: 'Saraksts ar visiem izmantotajiem elementiem vai rīkiem šajā skina versijā',
+    2: 'Šeit liekas diezgan klusi',
+    3: 'Ja tu esi izmantojis elementus no citiem skiniem vai zīmējumiem, lūdzu, pievieno tos augšējā laukā',
+    4: 'Nav grupēts'
+  },
+  checks: {
+    0: 'Viss',
+    1: 'Metadata',
+    2: 'Izceltais',
+    4: 'Pievieno vismaz vienu skina veidotāju',
+    5: 'Pievieno pāris tagu',
+    6: 'Skina lielums nav specificēts',
+    7: 'Izvēlies vismaz vienu spēles režīmu',
+    8: 'Izvēlies bildes izšķirtspēju (SD vai HD)',
+    9: 'Izvēlies vismaz vienu malu attiecību',
+    10: 'Nav ekrānuzņēmumu',
+    11: 'Pievieno vismaz vienu ekrānuzņēmumu ar gameplay',
+    12: 'Saite uz linku nav norādīta',
+    20: 'Neslikti',
+    21: 'Tu izlaboji visas problēmas. Malacis!',
+    22: 'Problēmu saraksts'
+  },
+  skins: {
+    0: 'Skini',
+    1: 'Populāri',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'Jaunākie',
+    5: 'Nejaušie',
+    6: 'Visi skini',
+    7: 'Malu attiecība',
+    8: 'Visi skini | osu skini',
+    9: 'Skins | Skina | Skinu',
+    10: 'osu Skini pēc {name} | Visi skini no',
+    11: 'Meklēt'
+  },
+  skin: {
+    0: 'Rediģēt',
+    1: 'Ekstra',
+    2: 'HD',
+    3: 'FullHD',
+    4: 'Animation',
+    5: 'Ekrānuzņēmumi',
+    6: 'Tikai gameplay',
+    7: 'Lejupielādes saite | Lejupielādēt skinu',
+    8: 'Foruma ziņa | osu! Forums',
+    9: 'Reddit post',
+    10: 'Veidotāji',
+    11: 'Tagi',
+    13: 'Video',
+    14: 'Jauna versija | Pārvaldīt versijas',
+    15: 'Atjaunināt | Iesūtīt',
+    16: 'Spēles režīmi',
+    17: 'Skina nosaukums',
+    18: 'Versija | Versijas',
+    19: 'Izmērs',
+    20: 'Metadata',
+    21: 'Vērtējums',
+    22: 'Lejupielādes',
+    23: 'Lejupielāde drīzumā būs pieejama',
+    24: 'Atlikušas {amount} sekundes',
+    25: 'Tu neesi viens no mums',
+    26: 'Šķiet, ka tev nav atļaujas rediģet šo skinu'
+  },
+  drag: {
+    0: 'Klikšķini šeit',
+    1: 'vai velc ekrānuzņēmumus šeit'
+  },
+  footer: {
+    0: 'Veidoja cyperdark',
+    1: 'Izmantotie materiāli'
+  },
+  credits: {
+    0: 'Chibi',
+    1: 'Mākslas darbi',
+    2: 'Ikonas',
+    3: 'Visas ikonas ir ņemtas no {name}',
+    4: 'Karogi',
+    5: 'Visas karogu bildes ir no {name}'
+  },
+  search: {
+    0: 'Meklēt skinu',
+    1: 'Skatījumi',
+    2: 'Tituls',
+    3: 'Vērtējums',
+    4: 'Izmērs',
+    5: 'Datums',
+    7: 'Nekas nav atrasts',
+    8: 'Izmēģini citus meklēšanas kritērijus',
+    9: 'Gaida pieprasījumu',
+    10: 'Izvēlies meklēsanas kritērijus vai vienkārši ieraksti skina nosaukumu, autoru vai tagu'
+  },
+  tabs: {
+    0: 'Meklēt',
+    1: 'Rediģēt',
+    2: 'Saraksts'
+  },
+  undefined: {
+    0: 'Nav nekā',
+    1: 'Diemžēl šajā lapā nekā nav, pamēģini paskatīties citās lapās',
+    2: 'Diemžēl šeit nekā nav',
+    3: 'Neautorizēts',
+    4: 'Tev vajag pieslēgties, lai rediģētu šo skinu.',
+    5: 'Tev vajag pieslēgties, lai pievienotu jaunu skinu.'
+  },
+  meta: {
+    title: {
+      0: 'Galvenā lapa',
+      1: 'Osu skinu kategorijas',
+      2: 'Populārie osu skini',
+      3: 'Meklēt osu skinus',
+      4: 'Tagu saraksts, «{name}»',
+      5: '{name} v{version} osu skins',
+      6: 'Izmantotie materiāli',
+      7: 'osu skini',
+      8: '{name} meklēt osu skinus',
+      9: 'Iedvesma',
+      10: '{name} veidoja osu skinus'
+    },
+    description: {
+      // 0: 'Get the best osu skins at our site, with a huge library to choose from! We have skins for different gamemodes and aspect ratios, and even feature top players and anime themes. Boost your gameplay with our high-quality skins.',
+      0: 'Get the best osu! skins at our site! Our huge library has skins for all gamemodes, aspect ratios and even top players. Plus, we have a special section for anime fans. Improve your gameplay with the perfect skin. #osuskins #osugame #anime',
+      1: '{views} views. An osu skin for {modes}, the size is {size}, created by {creators}. Supports {ratios}',
+      2: 'Find the perfect osu! skin with our advanced search feature. Browse through our huge library, filtered by gamemode, aspect ratio, and more. Or, check out skins used by top players.',
+      3: 'Discover a variety of game modes and aspect ratios on our site. Choose from standard, catch the beat, mania, and taiko modes. Find the perfect fit for your gaming preferences.',
+      4: 'Find the perfect osu skin from top players on our site. Browse our collection of popular skins and customize your gameplay. Discover a wide range of styles and themes to suit your preferences.',
+      5: 'List of existing tags for osu skins.',
+      6: '{name} osu skins collection!, free downloading',
+      7: 'A huge amount of osu skins for {name}, download for free!',
+      8: 'All osu skins created by {name}',
+      9: 'Find inspiration for your next osu! skin at our site. Browse through a diverse collection of skins and discover new techniques and ideas. Get inspired and create your own unique skins.'
+    },
+    keywords: { 0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins' }
+  },
+  buttons: {
+    0: 'Pieslēgties',
+    1: 'Galvenā lapa',
+    profile_osu: 'osu! Profils',
+    new_skin: 'Jauns skins',
+    notifications: 'Notifikācijas',
+    settings: 'Iestatījumi',
+    logout: 'Iziet',
+    load_more: 'Ielādēt vairāk',
+    copy_short: 'Kopēt saīsināto saiti',
+    copied: 'Nokopēts',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! Forums',
+    skin_return: 'Atpakaļ uz skinu'
+  },
+  user: {
+    roles: {
+      0: 'Veidotājs',
+      1: 'Miksētājs',
+      2: 'Animācijas',
+      3: 'Mākslas darbi',
+      4: 'Sadarbība'
+    }
+  },
+  tooltips: {
+    0: '{amount} skatījumi',
+    1: 'Atsvaidzini rezultātus',
+    2: 'Dilstoši | Augoši',
+    3: '{amount} likes',
+    4: 'Spied ctrl+enter, lai pievienotu tagu',
+    5: 'Rediģēt versiju',
+    6: '{s}Jāpieslēdzas{p}, lai varētu novērtēt šo skinu',
+    7: 'Nav īsā apraksta',
+    8: 'Nav saites'
+  },
+  lists: {
+    screens: {
+      0: 'Sveiciena ekrāns',
+      1: 'Main menu',
+      2: 'Song select',
+      3: 'Mode menu',
+      4: 'Mods',
+      5: 'Countdown',
+      6: 'Gameplay',
+      7: 'Spinner',
+      8: 'Section Pass',
+      9: 'Section Fail',
+      10: 'Pause Screen',
+      11: 'Fail Screen',
+      12: 'Result screen',
+      13: 'Skip',
+      14: 'Banner'
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed'
+    }
+  },
+  select: {
+    0: 'Jaunais',
+    1: 'Fixed',
+    2: 'Changed',
+    3: 'Removed',
+    4: 'Art',
+    5: 'Font',
+    6: 'Artist',
+    7: 'Skin element',
+    8: 'Audio/Sfx',
+    9: 'Tools',
+    views: 'Skatījumi',
+    likes: 'Likes',
+    size_range: 'Select size',
+    date_range: 'Select a date range',
+    ratio: 'Select aspect ratio | ratio'
+  },
+  placeholders: {
+    0: 'Choose roles',
+    1: 'Choose versions',
+    2: 'Type text...',
+    3: 'Select size',
+    4: 'Select date range',
+    5: 'Year',
+    6: 'Month',
+    7: 'Select aspect ratio',
+    8: 'Select categories',
+    9: 'Spēles režīmi',
+    10: 'Select type',
+    11: 'Paste a YouTube link',
+    12: 'Type description and url. Format description:url',
+    13: 'Select extra',
+    14: 'Paste a link',
+    15: 'Skin title, author, or tag'
+  }
+};
