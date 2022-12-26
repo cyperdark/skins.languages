117d82b1dd8462d2febf40e9f75eeb902870d264
owo <aburachenko2000@gmail.com>
2022-12-26 04:33:22 +0300

    polish

---
+++ pl.js
@@ -0,0 +1,355 @@
+export default {
+  hello: 'asd',
+
+  title: {
+    home: 'Strona główna',
+    guest: 'Zaloguj się',
+    lists: {
+      0: 'Listy',
+    },
+    tags: {
+      0: 'Tagi',
+      1: '{amount} tagi dla kategorii {name}',
+    },
+    ideas: {
+      0: 'Inspiracje',
+      1: '«{amount}»',
+    },
+    sounds: {
+      0: 'Dźwięki',
+    },
+    leaderboard: {
+      0: 'Rankingi',
+    },
+    extend: {
+      0: 'Więcej | Mniej',
+    },
+
+    skin: {
+      0: 'Ta skórka zawiera elementy z innych skórek',
+      1: 'Kliknij tutaj po więcej informacji',
+    },
+
+  },
+
+
+  changelogs: {
+    0: 'Lista zmian',
+    1: 'Lista wszystkich poprawek wprowadzonych w aktualnej wersji',
+    2: 'Trochę tu pusto.',
+    3: 'Ale możesz dodać zmiany wprowadzone do tej wersji skórki',
+    4: 'Nie pogrupowane',
+    5: 'Nowe',
+    6: 'Poprawki',
+    7: 'Zmiany',
+    8: 'Usunięcia',
+  },
+
+  creators: {
+    0: 'Twórcy',
+    1: 'Lista wszystkich twórców, którzy byli zaangażowani w tworzenie tej wersji',
+    2: 'Nikogo tu nie ma',
+    3: 'Wpisanie imienia gracza pokaże wyniki wyszukiwania osu.',
+    4: 'Edytor',
+    5: 'Role',
+    6: 'Wersje',
+  },
+
+  tags: {
+    0: 'Tagi',
+    1: 'Lista wszystkich tagów związanych z tą skórką',
+    2: 'Trochę tu pusto.',
+    3: 'Użyj pola powyżej, aby znaleźć nowe tagi',
+  },
+
+  videos: {
+    0: 'Filmiki',
+    1: 'Lista wszystkich prezentacji wideo dla tej skórki',
+    2: 'Zero prezentacji',
+    3: 'Wyszukaj na YouTube lub stwórz własną prezentację',
+  },
+
+  sources: {
+    0: 'Atrybucja',
+    1: 'Lista wszystkich używanych zasobów lub narzędzi w wersji skórki',
+    2: 'Trochę tu pusto.',
+    3: 'Jeśli wykorzystałeś zasoby z innych skórek lub grafik, pamiętaj o dodaniu ich w polu powyżej',
+    4: 'Nie pogrupowane',
+  },
+
+
+  checks: {
+    0: 'Wszystko',
+    1: 'Metadata',
+    2: 'Highlight',
+
+    4: 'Dodaj co najmniej jednego twórcę skórek',
+    5: 'Dodaj kilka tagów',
+    6: 'Nie określono wagi skórki',
+    7: 'Wybierz co najmniej jeden tryb gry',
+    8: 'Wybierz typ pliku (SD lub HD)',
+    9: 'Wybierz co najmniej jeden wymiar proporcji',
+    10: 'Brak zrzutów ekranu',
+    11: 'Dodaj co najmniej jeden zrzut ekranu z gry',
+    12: 'Brak linku do skórki',
+
+    20: 'Całkiem nieźle',
+    21: 'Każdy problem został rozwiązany, co jest całkiem dobre',
+    22: 'Lista kontrolna',
+  },
+
+  skins: {
+    0: 'Skórki',
+    1: 'Popularne',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'Ostatnie',
+    5: 'Losowe',
+    6: 'Wszystkie skórki',
+    7: 'Proporcje',
+    8: 'Wszystkie skórki | skórki osu',
+    9: 'Skórka | Skórki | Skórki',
+    10: 'Skórki osu autorstwa {name} | Wszystkie skórki od',
+    11: 'Szukaj',
+  },
+
+  skin: {
+    0: 'Edytuj',
+    1: 'Dodatkowe',
+    2: 'HD',
+    3: 'FullHD',
+    4: 'Animacje',
+    5: 'Zrzuty ekranu',
+    6: 'Zrzuty z gry',
+    7: 'Link do Pobrania | Pobierz skórkę',
+
+    8: 'Post na forum |osu! Forum',
+    9: 'Post na Reddicie',
+
+    10: 'Twórcy',
+    11: 'Tagi',
+    13: 'Filmiki',
+
+    14: 'Nowa wersja | Zarządzaj wersjami',
+    15: 'Aktualizuj | Zatwierdź',
+
+    16: 'Tryby gry',
+    17: 'Nazwa skórki',
+    18: 'Wersja | Wersje',
+    19: 'Rozmiar',
+    20: 'Metadata',
+
+    21: 'Oceny',
+    22: 'Pliki do pobrania',
+    23: 'Pobieranie będzie dostępne wkrótce',
+    24: 'Zostało {amount} sekund',
+  },
+
+  drag: {
+    0: 'Kliknij tutaj',
+    1: 'lub przeciągnij i upuść obrazy',
+  },
+
+  footer: {
+    0: 'Wykonane przez cyperdark',
+    1: 'Kredyty',
+  },
+
+  credits: {
+    0: 'Chibi',
+    1: 'Obrazy',
+    2: 'Ikony',
+    3: 'Wszystkie ikony pochodzą z {name}',
+    4: 'Flagi',
+    5: 'Wszystkie obrazy flag pochodzą z {name}'
+  },
+
+  search: {
+    0: 'Wyszukiwanie skórek',
+    1: 'Wyświetlenia',
+    2: 'Tytuł',
+    3: 'Oceny',
+    4: 'Rozmiar',
+    5: 'Data',
+    7: 'Nic nie znaleziono',
+    8: 'Spróbuj ustawić inne kryteria wyszukiwania',
+    9: 'Oczekiwanie na zapytanie',
+    10: 'Wybierz kryteria wyszukiwania lub po prostu wpisz tytuł skórki, autora lub tag',
+  },
+
+  tabs: {
+    0: 'Szukaj',
+    1: 'Edytuj',
+    2: 'Lista',
+  },
+
+  undefined: {
+    0: 'Pusto',
+    1: 'Niestety tutaj nic nie ma, ale możesz zajrzeć na inne strony',
+    2: 'Niestety nic tu nie ma',
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  meta: {
+    title: {
+      0: 'Strona główna',
+      1: 'Tryby gry, proporcje skórki osu',
+      2: 'Popularne skórki osu',
+      3: 'Szukaj skórek osu',
+      4: '«{name}» lista tagów',
+      5: '{name} v{version} skóra osu',
+      6: 'Kredyty',
+      7: 'skórki osu',
+      8: '{name} szukaj skórek osu',
+      9: 'Zainspiruj się innymi skórkami osu',
+      10: '{name} stworzone skórki osu',
+    },
+    description: {
+      // 0: 'Zdobądź najlepsze skórki osu na naszej stronie, z ogromną biblioteką do wyboru! Mamy skórki dla różnych trybów gry i proporcji, a nawet skórki najlepszych graczy i z motywami anime. Wzmocnij swoją rozgrywkę dzięki naszym skórkom o wysokiej jakości.',
+      0: 'Zdobądź najlepsze osu! skórki na naszej stronie! Nasza ogromna biblioteka zawiera skórki dla wszystkich trybów gry, proporcji, a nawet skórki najlepszych graczy. Ponadto mamy specjalną sekcję dla fanów anime. Wzmocnij swoją rozgrywkę dzięki jednej z naszych doskonałych skórek. #osuskins #osugame #anime',
+      1: '{views} wyświetleń. Skórka osu dla {modes}, jej waga to {size}, stworzona przez {creators}. Obsługuje {ratios}',
+      2: 'Znajdź idealne skórki osu! dzięki naszej zaawansowanej funkcji wyszukiwania. Przejrzyj naszą ogromną bibliotekę, filtrowaną według trybu gry, proporcji i więcej. Lub sprawdź skórki używane przez najlepszych graczy',
+      3: 'Odkryj różne tryby gry i proporcje na naszej stronie. Wybierz spośród trybu standardowego, ctb, mania i taiko. Znajdź idealne dopasowanie do swoich preferencji.',
+      4: 'Znajdź idealną skórkę osu od najlepszych graczy na naszej stronie. Przeglądaj naszą kolekcję popularnych skórek i dostosuj swoją rozgrywkę. Odkryj szeroką gamę stylów i motywów odpowiadających Twoim preferencjom.',
+      5: 'Lista istniejących tagów skórek osu.',
+      6: '{name} kolekcja skórek osu!, do pobrania za darmo',
+      7: 'Ogromna ilość skórek osu dla {name}, pobierz za darmo!',
+      8: 'Wszystkie skórki osu stworzone przez {name}',
+      9: 'Znajdź inspirację dla swojej następnej skórki osu! na naszej stronie. Przeglądaj różnorodną kolekcję skórek i odkrywaj nowe techniki i pomysły. Zainspiruj się i stwórz własne unikalne skórki.',
+    },
+    keywords: {
+      0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins'
+    },
+  },
+
+  buttons: {
+    profile_osu: 'Profil osu!',
+    new_skin: 'Nowa skórka',
+    notifications: 'Powiadomienia',
+    settings: 'Ustawienia',
+    logout: 'Wyloguj się',
+    load_more: 'Załaduj więcej',
+    copy_short: 'Skopiuj krótki link',
+    copied: 'Skopiowane',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'Forum osu!',
+    skin_return: 'Powrót do skórki',
+  },
+
+
+
+  user: {
+    roles: {
+      0: 'Twórca',
+      1: 'Mikser',
+      2: 'Animacje',
+      3: 'Sztuka',
+      4: 'Współpraca',
+    }
+  },
+
+  tooltips: {
+    0: '{amount} wyświetleń',
+    1: 'Odśwież wyniki',
+    2: 'Malejąco | Rosnąco',
+    3: '{amount} polubień',
+    4: 'Naciśnij ctrl+enter, aby dodać tag',
+    5: 'Edytuj wersję',
+    6: '{s}Zaloguj się{p}, aby móc ocenić tę skórkę',
+    7: 'Brak krótkiego opisu',
+    8: 'Brak adresu url',
+  },
+
+  lists: {
+    screens: {
+      0: 'Welcome',
+      1: 'Main menu',
+      2: 'Song select',
+      3: 'Mode menu',
+      4: 'Mods',
+      5: 'Countdown',
+      6: 'Gameplay',
+      7: 'Spinner',
+      8: 'Section Pass',
+      9: 'Section Fail',
+      10: 'Pause Screen',
+      11: 'Fail Screen',
+      12: 'Result screen',
+      13: 'Skip',
+      14: 'Banner',
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed',
+    }
+  },
+
+
+
+
+
+
+
+
+  select: {
+    0: 'Nowe',
+    1: 'Naprawione',
+    2: 'Zmienione',
+    3: 'Usunięte',
+
+    4: 'Sztuka',
+    5: 'Czcionka',
+    6: 'Artysta',
+    7: 'Element skórki',
+    8: 'Dźwięki/efekty dźwiękowe',
+    9: 'Narzędzia',
+
+
+
+    views: 'Wyświetlenia',
+    likes: 'Polubienia',
+    size_range: 'Wybierz rozmiar',
+    date_range: 'Wybierz zakres dat',
+    ratio: 'Wybierz proporcje | proporcje',
+  },
+
+  placeholders: {
+    0: 'Wybierz role',
+    1: 'Wybierz wersje',
+    2: 'Wpisz tekst...',
+    3: 'Wybierz rozmiar',
+    4: 'Wybierz zakres dat',
+    5: 'Rok',
+    6: 'Miesiąc',
+    7: 'Wybierz proporcje',
+    8: 'Wybierz kategorie',
+    9: 'Tryby gry',
+    10: 'Wybierz rodzaj',
+    11: 'Wklej link do youtube',
+    12: 'Wpisz opis i adres url. Format description:url',
+    13: 'Wybierz dodatkowe',
+    14: 'Wklej link',
+    15: 'Tytuł skórki, autor lub tag',
+  }
+};
