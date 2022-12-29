df6f6dda7ee39783f77e780e2caef3579e8b842e
cyperdark <aburachenko2000@gmail.com>
2022-12-29 17:22:44 +0300

    New translations en.js (Slovak)

---
+++ sk.js
@@ -0,0 +1,286 @@
+export default {
+  hello: 'ahoj',
+  title: {
+    home: 'Domovská stránka',
+    page: 'page',
+    guest: 'Prihlásenie',
+    lists: { 0: 'Zoznamy' },
+    tags: {
+      0: 'Tags',
+      1: '{amount} Značky pre kategóriu {name}'
+    },
+    ideas: {
+      0: 'Inšpirácia',
+      1: '«{amount}»'
+    },
+    sounds: { 0: 'Zvuky' },
+    leaderboard: { 0: 'Rebríčky' },
+    extend: { 0: 'Viac | Menej' },
+    skin: {
+      0: 'Tento skin obsahuje prvky z iných skinov',
+      1: 'Kliknite tu pre viac informácií'
+    }
+  },
+  changelogs: {
+    0: 'Zoznam zmien',
+    1: 'Zoznam všetkých vývojárov ktorí boli zapojení vo vývoji tejto verzie',
+    2: 'Nikto tu nie je',
+    3: 'Napísanie hráčov ho mena vám ukáže výsledky osu vyhľadávania',
+    4: 'Nezoskupené',
+    5: 'Nové',
+    6: 'Opravené',
+    7: 'Zmenené',
+    8: 'Odstránenia'
+  },
+  creators: {
+    0: 'Vývojári',
+    1: 'Zoznam všetkých vývojárov ktorí boli zapojení vo vývoji tejto verzie',
+    2: 'Nikto tu nieje',
+    3: 'Napísanie hráčov ho mena vám ukáže výsledky osu vyhľadávania.',
+    4: 'Editor',
+    5: 'Role',
+    6: 'Verzie'
+  },
+  tags: {
+    0: 'Tags',
+    1: 'List všetkých značiek súvisiacich s týmto skinom',
+    2: 'Tu to vyzerá prázdno.',
+    3: 'Použite horne pole pre nájdenie nových značok'
+  },
+  videos: {
+    0: 'Videa',
+    1: 'List všetkých video ukážok pre tento skin',
+    2: 'Žiadne ukážky',
+    3: 'Nájdite nejaké na YouTube alebo vytvorte vlastnú ukážku'
+  },
+  sources: {
+    0: 'Atribúcia',
+    1: 'List všetkých použitých prvkov a nástrojov v tejto verzii skinu',
+    2: 'Trochu prázdno tu',
+    3: 'Ak ste použili prvky z iných skinov alebo obrazov, pridajte ich do boxu hore',
+    4: 'Nezoskupené'
+  },
+  checks: {
+    0: 'Všetko',
+    1: 'Metadáta',
+    2: 'Zvýraznenie',
+    4: 'Pridajte aspoň jedného autora skinu',
+    5: 'Pridajte pár značiek',
+    6: 'Veľkosť skinu nie je špecifikovaná',
+    7: 'Vyberte aspoň jeden herný mód',
+    8: 'Vyberte typ skinu (SD alebo HD)',
+    9: 'Vyberte najmenej jeden pomer strán',
+    10: 'Žiadny snímok obrazovky',
+    11: 'Pridajte aspoň jeden snímok obrazovky z hrania',
+    12: 'Odkaz na skin nie je špecifikovaný',
+    20: 'Vyzerá to dobre',
+    21: 'Každý problém bol opravený, čo je dobre',
+    22: 'Kontrolná listina'
+  },
+  skins: {
+    0: 'Skiny',
+    1: 'Populárne',
+    2: 'std | ctb',
+    3: 'mania | taiko',
+    4: 'Aktuálne',
+    5: 'Náhodne',
+    6: 'Všetke skiny',
+    7: 'Pomer stran',
+    8: 'Všetke skiny | osu skiny',
+    9: 'Skin | Skiny | Skinny',
+    10: 'osu Skiny od {name} | Všetke skiny od',
+    11: 'Hľadať'
+  },
+  skin: {
+    0: 'Edit',
+    1: 'Extra',
+    2: 'HD',
+    3: 'FullHD',
+    4: 'Animation',
+    5: 'Screenshots',
+    6: 'Iba hrateľnosť',
+    7: 'Link na stiahnutie | stahovací link',
+    8: 'Príspevok na fóre |osu! Fórum',
+    9: 'Reddit príspevok',
+    10: 'Vývojári',
+    11: 'Tags',
+    13: 'Videa',
+    14: 'Nová Verzia | Spravovacia verzia',
+    15: 'Aktualizovať | Nahrať',
+    16: 'Herne mody',
+    17: 'Názov skinu',
+    18: 'Verzia | Verzie',
+    19: 'Velikost',
+    20: 'Metadáta',
+    21: 'Hodnotenie',
+    22: 'Stiahnutie',
+    23: 'Stiahnutie bude k dispozícii čoskoro',
+    24: '{amount} Zostávajúce sekundy',
+    25: 'Niesi jeden z nás',
+    26: 'Zdá sa, že nie ste autorom ani editorom tohto skinu'
+  },
+  drag: {
+    0: 'Klikni tu',
+    1: 'Alebo sem presuňte obrázky'
+  },
+  footer: {
+    0: 'Zrobil cyperdark',
+    1: 'Kredit'
+  },
+  credits: {
+    0: 'Chibi',
+    1: 'Umenie',
+    2: 'Ikony',
+    3: 'Všetky ikony sú prevzaté z {name}',
+    4: 'Vlajky',
+    5: 'Všetke obrázky vlajok sú od {name}'
+  },
+  search: {
+    0: 'Vyhľadať skin',
+    1: 'Pozretia',
+    2: 'Nazov',
+    3: 'Hodnotenie',
+    4: 'Velikost',
+    5: 'Dátum',
+    7: 'Nič sa nenašlo',
+    8: 'Skúste nastaviť iné filtre vyhľadávania',
+    9: 'Čaká sa na dotaz',
+    10: 'Vyberte vyhľadávacie filtre alebo napíšte názov skinu, autora alebo značku'
+  },
+  tabs: {
+    0: 'Hľadať',
+    1: 'Edit',
+    2: 'Zoznam'
+  },
+  undefined: {
+    0: 'Prázdny',
+    1: 'Bohužiaľ tu nič nie je, každopádne si môžete pozrieť iné stránky',
+    2: 'Bohužiaľ tu nič nie je',
+    3: 'Neoprávnené',
+    4: 'Ak chcete upraviť tento skin, musíte byť prihlásený.',
+    5: 'Musíte byt prihlásený ak chcete pridať novy skin.'
+  },
+  meta: {
+    title: {
+      0: 'Domovská stránka',
+      1: 'Herné režimy, pomery strán osu skinov',
+      2: 'Populárne osu skiny',
+      3: 'Vyhľadajte osu skiny',
+      4: 'Zoznam značiek, {name}',
+      5: '{name} v{version} osu skin',
+      6: 'Kredit',
+      7: 'osu skiny',
+      8: '{name} vyhľadaj osu skiny',
+      9: 'Inšpirujte sa inými osu skinmi',
+      10: '{name} výrobca osu skinou'
+    },
+    description: {
+      // 0: 'Get the best osu skins at our site, with a huge library to choose from! We have skins for different gamemodes and aspect ratios, and even feature top players and anime themes. Boost your gameplay with our high-quality skins.',
+      0: 'Získajte najlepšie osu! skiny na našej stránke! Naša obrovská knižnica má skiny pre všetke herné režimy, pomery strán a dokonca aj pre špičkových hráčov. Navyše máme špeciálnu sekciu pre fanúšikov anime. Zlepšite svoju hru s dokonalým vzhľadom. #osuskins #osugame #anime',
+      1: '{views} Zobrazenia. osu skin pre {modes}, veľkosť je {size}, vytvoril {creators}. Podporuje {ratios}',
+      2: 'Nájdite dokonali osu! skin s našou funkciou rozšíreného vyhľadávania. Pozrite si našu obrovskú knižnicu filtrovanú podľa herného režimu, pomeru strán a ďalších. Alebo si pozrite skiny používané špičkovými hráčmi.',
+      3: 'Objavte na našej stránke rôzne skiny pre rôzne herné režimy a pomery strán. Vyberte si zo štandardných režimov, zachyťte rytmus, mánie a režimy taiko. Nájdite to, čo dokonale vyhovuje vašim herným preferenciám.',
+      4: 'Nájdite dokonalý osu skin od najlepších hráčov na našej stránke. Pozrite si našu kolekciu obľúbených skinou a prispôsobte si svoju hru. Objavte širokú škálu štýlov a tém, ktoré vyhovujú vašim preferenciám.',
+      5: 'Zoznam existujúcich značiek pre osu skiny.',
+      6: '{name} kolekcia osu skinov!, bezplatné stiahnutie',
+      7: 'Obrovské množstvo osu skinov pre {name}, stiahnite si zadarmo!',
+      8: 'Všetke osu skiny vytvoril {name}',
+      9: 'Nájdite inšpiráciu pre svoje ďalšie osu! skiny na našej stránke. Pozrite si rozmanitú kolekciu vzhľadov a objavte nové techniky a nápady. Nechajte sa inšpirovať a vytvorte si vlastné jedinečné skiny.'
+    },
+    keywords: { 0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins' }
+  },
+  buttons: {
+    0: 'Login',
+    1: 'Domovská stránka',
+    profile_osu: 'osu! Profile',
+    new_skin: 'Novy skin',
+    notifications: 'Oznámenia',
+    settings: 'Nastavenia',
+    logout: 'Odhlásiť sa',
+    load_more: 'Načítať viac',
+    copy_short: 'Skopírovať krátky link',
+    copied: 'Skopírované',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! Forum',
+    skin_return: 'Späť na skin'
+  },
+  user: {
+    roles: {
+      0: 'Kreator',
+      1: 'Mixer',
+      2: 'Animácie',
+      3: 'Umenie',
+      4: 'Spolupráca'
+    }
+  },
+  tooltips: {
+    0: '{amount} zobrazenie',
+    1: 'Obnoviť výsledky',
+    2: 'Klesajúci | Stúpajúci',
+    3: '{amount} obľúbených',
+    4: 'Stlačte ctrl+enter pre pridanie označenia',
+    5: 'Upraviť verziu',
+    6: '{s}Prihláste sa{p}, aby ste mohli ohodnotiť tento skin',
+    7: 'Žiadny krátky popis',
+    8: 'Žiadne URL'
+  },
+  lists: {
+    screens: {
+      0: 'Vitaj',
+      1: 'Hlavné menu',
+      2: 'Vyber pesničky',
+      3: 'Menu mody',
+      4: 'Mody',
+      5: 'Odpočet',
+      6: 'Hra',
+      7: 'Spinner',
+      8: 'Sekcia dokončenia',
+      9: 'Sekcia nedokončená',
+      10: 'Obrazovka Pauzy',
+      11: 'Obrazovka prehrania',
+      12: 'Obrazovka výsledkov',
+      13: 'Preskočiť',
+      14: 'Banner'
+    },
+    cats: {
+      0: 'NSFW',
+      1: 'Furry',
+      2: 'Mixed'
+    }
+  },
+  select: {
+    0: 'Nové',
+    1: 'Opravené',
+    2: 'Zmenené',
+    3: 'Odstránené',
+    4: 'Obrázky',
+    5: 'Font',
+    6: 'Umelec',
+    7: 'Elementy skinu',
+    8: 'Audio/Sfx',
+    9: 'Nástroje',
+    views: 'Pozretia',
+    likes: 'Obľúbene',
+    size_range: 'Vyber velikosti',
+    date_range: 'Vyber časového obdobia',
+    ratio: 'Vyber pomer stran | pomer'
+  },
+  placeholders: {
+    0: 'Vyber rolu',
+    1: 'Vyber verziu',
+    2: 'Napíš text...',
+    3: 'Vyber velikosti',
+    4: 'Výber časového obdobia',
+    5: 'Rok',
+    6: 'Mesiac',
+    7: 'Vyber pomer stran',
+    8: 'Vyberte kategóriu',
+    9: 'Herne mody',
+    10: 'Vložte typ',
+    11: 'Vložte Youtube odkaz',
+    12: 'Zadajte popis a url.Popis formátu:url',
+    13: 'Výber extra',
+    14: 'Vložte link',
+    15: 'Názov skinu, autora, alebo označenie'
+  }
+};
