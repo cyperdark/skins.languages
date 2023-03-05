827e9644739f17949acdbd379f335f265aa69927
cyperdark <aburachenko2000@gmail.com>
2023-03-05 07:42:44 +0300

    New translations en.js (Chinese Simplified)

---
+++ zhcn.js
@@ -0,0 +1,300 @@
+export default {
+  title: {
+    home: '主页',
+    login: '登录',
+    lists: { 0: '列表' },
+    tags: {
+      0: '标签',
+      1: '{amount} 个标签属于 {name} 类别'
+    },
+    ideas: {
+      0: '灵感',
+      1: '«{amount}»'
+    },
+    sounds: { 0: '音效' },
+    leaderboard: { 0: '排行榜' },
+    extend: { 0: '更多 | 更少' },
+    skin: {
+      0: '此皮肤包含来自其他皮肤的元素',
+      1: '点击这里获取更多信息'
+    }
+  },
+  changelogs: {
+    0: '更新日志',
+    1: '当前版本完成的所有修订列表',
+    2: '这里什么也没有',
+    3: '但您可以添加您对此版本的皮肤所作的更改',
+    4: '未分组',
+    5: '新增',
+    6: '已修复',
+    7: '更改',
+    8: '移除'
+  },
+  creators: {
+    0: '作者',
+    1: '列出所有参与此版本制作的创作者',
+    2: '这里没有人',
+    3: '使用玩家名称搜索将显示一个玩家的个人资料列表',
+    4: '编辑者',
+    5: '角色',
+    6: '版本'
+  },
+  tags: {
+    0: '标签',
+    1: '与此皮肤相关的所有标签列表',
+    2: '这里似乎什么也没有',
+    3: '使用上面的字段查找新标签'
+  },
+  videos: {
+    0: '视频',
+    1: '此皮肤的所有的演示视频列表',
+    2: '没有演示视频',
+    3: '在 YouTube 上搜索或创建您自己的演示视频'
+  },
+  sources: {
+    0: '属性',
+    1: '在此版本的皮肤中所有使用过的资源或工具列表',
+    2: '此处什么也没有',
+    3: '如果您使用了来自其他皮肤的资源或艺术作品，请务必将其添加到上面的框中',
+    4: '未分组'
+  },
+  checks: {
+    0: '全部',
+    1: '元数据',
+    2: '高光',
+    4: '添加至少一个皮肤作者',
+    5: '添加多个标签',
+    6: '皮肤大小未指定',
+    7: '选择至少一个游戏模式',
+    8: '选择图像分辨率(SD 或 HD)',
+    9: '选择至少一个宽高比',
+    10: '未提供截图',
+    11: '添加至少一张游戏截图',
+    12: '皮肤链接未指定',
+    20: '看起来非常棒',
+    21: '每个问题都已解决，干得好',
+    22: '检查清单'
+  },
+  skins: {
+    0: '皮肤',
+    1: '热门',
+    2: '戳泡泡 | 接水果',
+    3: '下落式 | 太鼓',
+    4: '最新',
+    5: '随机',
+    6: '所有皮肤',
+    7: '宽高比',
+    8: '所有皮肤 | osu! 皮肤',
+    9: '皮肤 | 皮肤 | 皮肤',
+    10: '{name} 制作的 osu 皮肤 | 所有来自... 皮肤',
+    11: '搜索'
+  },
+  skin: {
+    0: '编辑',
+    1: '更多',
+    2: 'HD',
+    3: 'FullHD',
+    4: '动画',
+    5: '截图',
+    6: '仅游戏画面',
+    7: '下载链接 | 下载皮肤',
+    8: '论坛帖子 | osu! 论坛',
+    9: 'Reddit 帖子',
+    10: '作者',
+    11: '标签',
+    13: '视频',
+    14: '新版本 | 管理版本',
+    15: '更新 | 提交',
+    16: '游戏模式',
+    17: '皮肤名称',
+    18: '版本 | 版本',
+    19: '尺寸',
+    20: '元数据',
+    21: '评分',
+    22: '下载',
+    23: '下载即将可用',
+    24: '剩余 {amount} 秒',
+    25: '您不是我们的成员',
+    26: '看起来您不是此皮肤的作者或编辑者',
+    27: '皮肤由'
+  },
+  drag: {
+    0: '点击这里',
+    1: '或者拖放图像到这里'
+  },
+  footer: {
+    0: '由cyperdark制作',
+    1: '版权'
+  },
+  credits: {
+    0: 'Chibi',
+    1: '艺术品',
+    2: '图标',
+    3: '所有图标来自 {name}',
+    4: '旗帜',
+    5: '所有旗帜图像都来自 {name}',
+    6: '本地化',
+    7: '有人吗？[{lang}]',
+    8: '似乎 cpol 忘记添加了它们。{text}'
+  },
+  search: {
+    0: '皮肤查询',
+    1: '视图',
+    2: '标题',
+    3: '评分',
+    4: '尺寸',
+    5: '日期',
+    7: '未找到任何内容',
+    8: '请尝试设置不同的搜索过滤器',
+    9: '待查询中',
+    10: '使用搜索过滤器或只写入皮肤标题、作者或标签',
+    11: '下载',
+    12: '名称'
+  },
+  tabs: {
+    0: '搜索',
+    1: '编辑',
+    2: '列表'
+  },
+  undefined: {
+    0: '空',
+    1: '很不幸，这里没有任何东西，您可以查看其他页面',
+    2: '很不幸，这里没有任何内容',
+    3: '无权限',
+    4: '你需要登录才能编辑此皮肤。',
+    5: '你需要登录才能添加新的皮肤。'
+  },
+  meta: {
+    title: {
+      0: '发现 osu 皮肤',
+      1: '游戏模式、画面比例 osu 皮肤',
+      2: '热门的 osu! 皮肤',
+      3: '搜索 osu! 皮肤',
+      4: '标签列表， {name}',
+      5: '{name} v{version} osu 皮肤',
+      6: '版权',
+      7: 'osu! 皮肤',
+      8: '{name} 搜索 osu 皮肤',
+      9: '从其他 osu 皮肤中获得灵感',
+      10: '{name} 制作的 osu 皮肤'
+    },
+    description: {
+      0: '在我们的网站上获取最佳 osu! 皮肤！我们的巨大库中有所有游戏模式、画面比例甚至是顶尖玩家的皮肤。此外，我们还有一个专门的动漫粉丝区。使用完美的皮肤提高你的游戏体验。#osuskins #osugame #anime',
+      1: '浏览{views} 次。这是一个针对{modes} 模式的osu皮肤，大小为{size}，由{creators} 制作。支持{ratios} 画面比例。',
+      2: '使用我们高级的搜索功能找到完美的osu!皮肤。浏览我们庞大的库，按游戏模式，画面比例等进行筛选。或者，查看顶尖玩家使用的皮肤。',
+      3: '在我们的网站上发现各种各样不同游戏模式和画面比例的皮肤。从标准模式，追踪音符，狂热和太鼓模式中选择。找到适合你游戏喜好的完美皮肤。',
+      4: '在我们的网站上找到顶尖玩家的完美osu皮肤。浏览我们的流行皮肤收藏，并自定义您的游戏体验。发现各种各样的风格和主题，以满足您的喜好。',
+      5: 'osu皮肤的现有标签列表。',
+      6: '{name} osu 皮肤收藏！免费下载',
+      7: '大量{name} osu皮肤，免费下载！',
+      8: '{name} 制作的所有 osu! 皮肤',
+      9: '在我们的网站上为您的下一个 osu! 皮肤寻找灵感。浏览多样化的皮肤集合，发现新技巧和想法。受到启发并创建自己独特的皮肤。'
+    },
+    keywords: { 0: 'osu, osu!, osu skins, std skins, ctb skins, mania skins, taiko skins, mrekk osu skins,Rafis osu skins,FlyingTuna osu skins,Vaxei osu skins,Cookiezi osu skins,Varvalian osu skins,RyuK osu skins,WhiteCat osu skins,_RyuK osu skins,BeasttrollMC osu skins,Genshin Impact osu skins,Mathi osu skins,WubWoofWolf osu skins,Merami osu skins,xootynator osu skins,Gayzmcgee osu skins,[Karcher] osu skins,Bubbleman osu skins,ChomikBox osu skins,NSFW osu skins,Lifeline osu skins,Hatsune Miku osu skins,Azerite osu skins,Umbre osu skins,404 AimNotFound osu skins,-Nikstrim osu skins,bloo osu skins,Shigetora osu skins,Badeu osu skins,FunOrange osu skins,worst hr player osu skins,Tekkito osu skins,ninerik osu skins,azr8 osu skins,idke osu skins,Andros osu skins,Zylice osu skins,Rektygon osu skins,Komori osu skins,anime osu skins,cyperdark osu skins,Red_Pixel osu skins,Utami osu skins,Arknights osu skins,Karthy osu skins,Paraqeet osu skins,aetrna osu skins,Spare osu skins,-GN osu skins,Mastasz osu skins,Seouless osu skins,haga1115 osu skins,shimon osu skins,fieryrage osu skins,Ekoro osu skins,Firebat92 osu skins,_Shield osu skins,-Roaz- osu skins,- Dark - osu skins,atturbo555 osu skins,Aristia osu skins,_Aiko__ osu skins,z x c osu skins,eternum osu skins,talala osu skins,NyanPotato osu skins,Arnold24x24 osu skins,Alumetri osu skins,im a fancy lad osu skins,jordanlr7 osu skins,Woey osu skins,khz osu skins,rrtyui osu skins,kieg osu skins,Rupertion osu skins,FGSky osu skins,Aireu osu skins,Neko God osu skins,Apostol osu skins,Abyssal osu skins,dokidokilolixx osu skins,Mlaw osu skins,Ponytail osu skins,Reedkatt osu skins,Zestiny osu skins,Apraxia osu skins,Binfy osu skins,Rohulk osu skins,-PloX osu skins,DuyKhang-sama osu skins,BronyPH osu skins,Intercambing osu skins,Xeltol osu skins,Valorant osu skins,Akolibed osu skins,hololive osu skins,kablaze osu skins,tokyo ghoul osu skins,Trail Mix osu skins,Dereban osu skins' }
+  },
+  buttons: {
+    0: '登录',
+    1: '主页',
+    profile_osu: 'osu! 个人资料',
+    new_skin: '新皮肤',
+    notifications: '通知',
+    settings: '设置',
+    logout: '注销',
+    load_more: '显示更多',
+    copy_short: '复制短链接',
+    copied: '已复制',
+    reddit_link: 'Reddit',
+    osu_forum_link: 'osu! 论坛',
+    skin_return: '返回皮肤'
+  },
+  user: {
+    roles: {
+      0: '创建者',
+      1: '混合器',
+      2: '动画',
+      3: '艺术品',
+      4: '合作'
+    }
+  },
+  tooltips: {
+    0: '观看次数：{amount}',
+    1: '刷新结果',
+    2: '降序 | 升序',
+    3: '喜欢: {likes} / 不喜欢: {dislikes}',
+    4: '按住 ctrl+回车键 添加标签',
+    5: '编辑版本',
+    6: '{s}登录{p}，才能对这个皮肤进行评分。',
+    7: '没有简短描述',
+    8: '没有网址',
+    9: '下载次数: {amount}'
+  },
+  lists: {
+    screens: {
+      0: '欢迎',
+      1: '主菜单',
+      2: '歌曲选择',
+      3: '模式菜单',
+      4: 'Mods',
+      5: '倒计时',
+      6: '玩法',
+      7: '微调器',
+      8: '部分通过',
+      9: '部分失败',
+      10: '暂停画面',
+      11: '失败画面',
+      12: '结算画面',
+      13: '跳过',
+      14: '横幅'
+    },
+    cats: {
+      0: 'NSFW',
+      1: '福瑞',
+      2: '结合'
+    }
+  },
+  select: {
+    0: '新增',
+    1: '已修复',
+    2: '更改',
+    3: '已删除',
+    4: '艺术',
+    5: '字体',
+    6: '画师',
+    7: '皮肤元素',
+    8: '音效',
+    9: '工具',
+    views: '视图',
+    likes: '喜欢',
+    dislikes: '不喜欢',
+    downloads: '下载',
+    size_range: '选择大小',
+    date_range: '选择一个日期范围',
+    ratio: '选择宽高比'
+  },
+  placeholders: {
+    0: '选择角色',
+    1: '选择版本',
+    2: '输入文本...',
+    3: '选择大小',
+    4: '选择日期范围',
+    5: '年',
+    6: '月',
+    7: '选择宽高比',
+    8: '选择类别',
+    9: '游戏模式',
+    10: '选择类型',
+    11: '粘贴一个 YouTube 链接',
+    12: '类型描述和 url. 格式描述:url',
+    13: '选择额外的',
+    14: '粘贴链接',
+    15: '皮肤标题、作者或标签以搜索'
+  },
+  contribute: { 0: '贡献' },
+  nsfw: {
+    0: '警告！',
+    1: '此皮肤包含敏感或成人内容',
+    2: '此皮肤含有敏感或成人内容，某些用户可能发现令人不安的',
+    3: ' 好吧',
+    4: ' bye o/'
+  }
+};
