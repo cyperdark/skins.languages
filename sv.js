1f068517929ae0361fdfd2162a8d0caf86096723
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:17 +0300

    New translations en.js (Swedish)

--- sv.js
+++ sv.js
@@ -33,7 +33,7 @@ export default {
     8: 'Tar bort'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Ingenting hittat',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
