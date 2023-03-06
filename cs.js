33c2a2dcb3a61e08b9c407502cfe80ea53575541
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:01 +0300

    New translations en.js (Czech)

--- cs.js
+++ cs.js
@@ -33,7 +33,7 @@ export default {
     8: 'Odstranit'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Nic nebylo nalezeno',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
