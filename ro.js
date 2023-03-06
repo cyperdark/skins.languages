31112a9ec8e608998747d2365f7f218ae975d79a
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:12:54 +0300

    New translations en.js (Romanian)

--- ro.js
+++ ro.js
@@ -33,7 +33,7 @@ export default {
     8: 'Desființări'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Nu am găsit nimic',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
