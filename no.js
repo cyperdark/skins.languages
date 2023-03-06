f5c86a624f55d47e9e22bf050c0fc88ff394911a
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:12 +0300

    New translations en.js (Norwegian)

--- no.js
+++ no.js
@@ -33,7 +33,7 @@ export default {
     8: 'Fjerning'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Ingenting funnet',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
