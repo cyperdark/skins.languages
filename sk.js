abc75ac494b4041e3fe3b0f0096fd74b526da023
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:15 +0300

    New translations en.js (Slovak)

--- sk.js
+++ sk.js
@@ -33,7 +33,7 @@ export default {
     8: 'Odstránenia'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Nič sa nenašlo',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
