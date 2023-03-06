0c0603c4ce3eda42d5fb49a1f768afbb12d92b19
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:25 +0300

    New translations en.js (Malay)

--- ms.js
+++ ms.js
@@ -33,7 +33,7 @@ export default {
     8: 'Pembuangan'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Tiada apa-apa dijumpai',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
