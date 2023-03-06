c026fcf43de7916c64def3a5c62bfece4363c189
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:12:59 +0300

    New translations en.js (Bulgarian)

--- bg.js
+++ bg.js
@@ -33,7 +33,7 @@ export default {
     8: 'Премахнато'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Нищо не е намерено',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
