7edda253651c11eb9e19c57bf2c83eaf7beb897e
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:24 +0300

    New translations en.js (Latvian)

--- lv.js
+++ lv.js
@@ -33,7 +33,7 @@ export default {
     8: 'Izņemtais'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Nekas nav atrasts',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
