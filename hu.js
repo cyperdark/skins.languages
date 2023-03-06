76fa95dc4e49626faf1b65897db63d3a64364aeb
cyperdark <aburachenko2000@gmail.com>
2023-03-06 15:13:06 +0300

    New translations en.js (Hungarian)

--- hu.js
+++ hu.js
@@ -33,7 +33,7 @@ export default {
     8: 'Eltávolítások'
   },
   ideas: {
-    0: 'Nothing founded',
+    0: 'Nincs találat',
     1: 'Screenshots of «{type}» were not uploaded to the site'
   },
   creators: {
