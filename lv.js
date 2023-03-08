3b9f32f03c1aa166ac9b5aba68f28c358bf0e2af
cyperdark <aburachenko2000@gmail.com>
2023-03-08 09:45:47 +0300

    New translations en.js (Latvian)

--- lv.js
+++ lv.js
@@ -25,7 +25,7 @@ export default {
     0: 'Izmaiņu saraksts',
     1: 'Saraksts ar visām veiktajām revīzijām pašreizējā versijā',
     2: 'Šeit nekā nav',
-    3: 'Bet tu vari pievienot izmaiņas, kuras tu veici šai skina versijai',
+    3: 'But you can add the changes you made\'ve to this version of the skin',
     4: 'Nav grupēts',
     5: 'Jaunais',
     6: 'Labotais',
@@ -81,8 +81,8 @@ export default {
     10: 'Nav ekrānuzņēmumu',
     11: 'Pievieno vismaz vienu ekrānuzņēmumu ar gameplay',
     12: 'Saite uz linku nav norādīta',
-    20: 'Neslikti',
-    21: 'Tu izlaboji visas problēmas. Malacis!',
+    20: 'Looks pretty good',
+    21: 'Every issue was fixed. Good job!',
     22: 'Problēmu saraksts',
     23: 'Add at least one public version'
   },
@@ -151,7 +151,7 @@ export default {
     5: 'Visas karogu bildes ir no {name}',
     6: 'Tulkošana',
     7: 'Neviens? [{lang}]',
-    8: 'Šķiet, ka cpol aizmirsa viņus pievienot. {text}'
+    8: 'Seems like cyperdark forgot to add them. {text}'
   },
   search: {
     0: 'Meklēt skinu',
@@ -161,9 +161,9 @@ export default {
     4: 'Izmērs',
     5: 'Datums',
     7: 'Nekas nav atrasts',
-    8: 'Izmēģini citus meklēšanas kritērijus',
+    8: 'Try using different search filters',
     9: 'Gaida pieprasījumu',
-    10: 'Izvēlies meklēsanas kritērijus vai vienkārši ieraksti skina nosaukumu, autoru vai tagu',
+    10: 'Use the search filters or type the skin\'s title, author or tags',
     11: 'Lejupielādes',
     12: 'Nosaukums',
     13: {
@@ -180,11 +180,11 @@ export default {
   },
   undefined: {
     0: 'Nav nekā',
-    1: 'Diemžēl šajā lapā nekā nav, pamēģini paskatīties citās lapās',
-    2: 'Diemžēl šeit nekā nav',
+    1: 'Unfortunately, there\'s nothing here, but you can take a look at the other pages',
+    2: 'Unfortunately, there\'s nothing here',
     3: 'Neautorizēts',
-    4: 'Tev vajag pieslēgties, lai rediģētu šo skinu.',
-    5: 'Tev jāpieslēdzas, lai pievienotu jaunu skinu.'
+    4: 'You need to be logged in order to edit this skin.',
+    5: 'You need to be logged in order to add a new skin.'
   },
   meta: {
     title: {
@@ -247,7 +247,7 @@ export default {
     3: 'Patīk: {likes} / Nepatīk: {dislikes}',
     4: 'Spied ctrl+enter, lai pievienotu tagu',
     5: 'Rediģēt versiju',
-    6: '{s}Jāpieslēdzas{p}, lai varētu novērtēt šo skinu',
+    6: '{s}Log in{p}, in order to rate this skin',
     7: 'Nav īsā apraksta',
     8: 'Nav saites',
     9: 'Lejupielādes: {amount}'
@@ -296,9 +296,9 @@ export default {
     ratio: 'Malu attiecība | attiecība'
   },
   placeholders: {
-    0: 'Atlasi lomas',
-    1: 'Izvēlies versijas',
-    2: 'Ievadi tekstu...',
+    0: 'Select roles',
+    1: 'Select versions',
+    2: 'Type some text...',
     3: 'Skina izmērs',
     4: 'Datuma diapazons',
     5: 'Gads',
@@ -308,12 +308,12 @@ export default {
     9: 'Spēles režīmi',
     10: 'Izvēlies tipu',
     11: 'Ielīmē YouTube saiti',
-    12: 'Ievadi aprakstu un saiti. Piemērs: apraksts:saite',
+    12: 'Type a description and an url. Format description:url',
     13: 'Izvēlies vairāk',
     14: 'Ielīmē saiti',
-    15: 'Skina nosaukums, veidotājs vai tags',
+    15: 'Skin title, author, or tags',
     16: 'Select category',
-    17: 'Type name of the tag'
+    17: 'Type the name of the tag'
   },
   contribute: { 0: 'Izpalīdzēt' },
   nsfw: {
@@ -325,12 +325,12 @@ export default {
   },
   removed: {
     0: 'Version unavailable',
-    1: 'Author of the skin decided to remove this version from the site.',
-    2: 'Switch to available version'
+    1: 'The author of the skin decided to remove this version from the site.',
+    2: 'Switch to an available version'
   },
   notify: {
-    0: 'Issues not fixed',
-    1: 'Not all required fields was filled',
+    0: 'Issues were not fixed',
+    1: 'Not all required fields were filled',
     2: 'Show',
     3: 'Not found',
     4: 'Banner not found (Currently, the first screenshot is being used as the banner)',
@@ -339,7 +339,7 @@ export default {
     7: 'ID not found',
     8: 'ID of the video not found, try another link (At the moment only YouTube is supported)',
     9: 'Oops',
-    10: 'Looks like your session is expired, open new page and log into your account (Don\'t worry, everything is saved automatically)',
+    10: 'Looks like your session expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
     11: 'Exists',
     12: 'This video has already been added',
     13: 'Exists',
@@ -354,33 +354,33 @@ export default {
     22: 'New creator has been added',
     23: 'HUH',
     24: 'Impossible to remove a deleted creator',
-    25: 'Exists',
-    26: 'Same change with this type has been added earlier',
-    27: 'Warning',
-    28: 'Same url with this type has been added earlier',
+    25: 'Duplicate',
+    26: 'This change has already been added earlier.',
+    27: 'Duplicate',
+    28: 'This url has already been added earlier.',
     29: 'Warning',
-    30: 'You forgot to add a description for a url. Example: Description:url',
+    30: 'You forgot to add a description for the url. Example: Description:url',
     31: 'Warning',
-    32: 'You forgot to add a url for description. Example: Description:url',
+    32: 'You forgot to add an url for the description. Example: Description:url',
     33: 'Restored',
-    34: 'Version {name} has been restored',
+    34: 'Version «{name}» has been restored',
     35: 'Deleted',
-    36: 'Version {name} has been deleted',
+    36: 'Version «{name}» has been deleted',
     37: 'Error',
-    38: 'Seems like the server thinks you\'re a bot',
-    39: 'Looks like you\'re user session is expired, open new page and log into your account (Don\'t worry, everything is saved automatically)',
-    40: 'Mismatched credentials (re:login in account. Don\'t worry, everything is saved automatically)',
+    38: 'Apparently the server thinks you\'re a robot.',
+    39: 'Looks like your user session has expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
+    40: 'Mismatched credentials (relogin in account. Don\'t worry, everything was saved automatically)',
     41: 'Unknown error',
-    42: 'You cannot edit inexistent skin',
-    43: 'Seems like you don\'t have permissions to edit this skin',
-    44: 'You cannot submit this version as it is the same as the previous one',
-    45: 'Those changes has been sent already on moderation',
+    42: 'You cannot edit a nonexistent skin',
+    43: 'Seems like you don\'t have permission to edit this skin',
+    44: 'You cannot send this version because it is the same as the previous one.',
+    45: 'These changes have already been sent for moderation',
     46: 'Success',
     47: 'New changes have been submitted for moderation',
     48: 'Changes have been updated in moderation',
     49: 'Odd link',
     50: 'This link cannot be used as a download link',
-    51: 'Submitted to moderation'
+    51: 'Submitted for moderation'
   },
   restricted_view: {
     0: 'Neautorizēts',
