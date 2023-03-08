6641446907052b18566e37f7439b168015925973
cyperdark <aburachenko2000@gmail.com>
2023-03-08 09:45:35 +0300

    New translations en.js (Norwegian)

--- no.js
+++ no.js
@@ -25,7 +25,7 @@ export default {
     0: 'Endringslogg',
     1: 'Liste over alle endringene som er gjort i den gjeldende versjonen',
     2: 'Ingenting her',
-    3: 'Men du kan legge til endringene du har gjort av skinnet',
+    3: 'But you can add the changes you made\'ve to this version of the skin',
     4: 'Ikke gruppert',
     5: 'Ny',
     6: 'Rettelser',
@@ -81,8 +81,8 @@ export default {
     10: 'Ingen skjermbilder postet',
     11: 'Legg til hvertfall et skjermbilde av skinnet i bruk',
     12: 'Lenke til skinnet er ikke spesifisert',
-    20: 'Ser bra ut',
-    21: 'En hver feil var fikset. Godt jobba',
+    20: 'Looks pretty good',
+    21: 'Every issue was fixed. Good job!',
     22: 'Sjekkliste',
     23: 'Add at least one public version'
   },
@@ -151,7 +151,7 @@ export default {
     5: 'Alle flagg bilder er fra {name}',
     6: 'Localisation',
     7: 'Anyone? [{lang}]',
-    8: 'Seems like, cpol forget to add them. {text}'
+    8: 'Seems like cyperdark forgot to add them. {text}'
   },
   search: {
     0: 'Skin søk',
@@ -161,9 +161,9 @@ export default {
     4: 'Størrelse',
     5: 'Dato',
     7: 'Ingenting funnet',
-    8: 'Prøv å bruk et annet søkefilter',
+    8: 'Try using different search filters',
     9: 'Venter godkjenning',
-    10: 'Velg søkefilter eller skriv tittelten til skinnet, skaper av skin eller en tag',
+    10: 'Use the search filters or type the skin\'s title, author or tags',
     11: 'Nedlasting',
     12: 'Name',
     13: {
@@ -180,11 +180,11 @@ export default {
   },
   undefined: {
     0: 'Tomt',
-    1: 'Her var det dessverre ingenting, uansett så kan du se på våre andre sider',
-    2: 'Her var det dessverre ingenting',
+    1: 'Unfortunately, there\'s nothing here, but you can take a look at the other pages',
+    2: 'Unfortunately, there\'s nothing here',
     3: 'Ikke autorisert',
-    4: 'Du må være innlogget for å redigere dette skinnet.',
-    5: 'You need to be logged in to add a new skin.'
+    4: 'You need to be logged in order to edit this skin.',
+    5: 'You need to be logged in order to add a new skin.'
   },
   meta: {
     title: {
@@ -247,7 +247,7 @@ export default {
     3: 'Likes: {likes} / Dislikes: {dislikes}',
     4: 'Press ctrl+enter for å legge til en tagg',
     5: 'Rediger versjon',
-    6: '{s}Logg inn{p}, for å legge igjen en vurdering om dette skinnet',
+    6: '{s}Log in{p}, in order to rate this skin',
     7: 'Ingen liten beskrivelse',
     8: 'Ingen link',
     9: 'Downloads: {amount}'
@@ -296,9 +296,9 @@ export default {
     ratio: 'Velg størrelsesformat | format'
   },
   placeholders: {
-    0: 'Velg roller',
-    1: 'Velg versjoner',
-    2: 'Skriv tekst...',
+    0: 'Select roles',
+    1: 'Select versions',
+    2: 'Type some text...',
     3: 'Velg størrelse',
     4: 'Velg dato ',
     5: 'År',
@@ -308,12 +308,12 @@ export default {
     9: 'Spillmodus',
     10: 'Velg type',
     11: 'Lim inn en YouTube lenke',
-    12: 'Skriv beskrivelse og url. Format beskrivelsen:url',
+    12: 'Type a description and an url. Format description:url',
     13: 'Velg ekstra',
     14: 'Lim inn lenke',
-    15: 'Skin tittel, skaper, eller tag',
+    15: 'Skin title, author, or tags',
     16: 'Select category',
-    17: 'Type name of the tag'
+    17: 'Type the name of the tag'
   },
   contribute: { 0: 'Contribute' },
   nsfw: {
@@ -325,12 +325,12 @@ export default {
   },
   removed: {
     0: 'Version unavailable',
-    1: 'Author of the skin decided to remove this version from the site.',
-    2: 'Switch to available version'
+    1: 'The author of the skin decided to remove this version from the site.',
+    2: 'Switch to an available version'
   },
   notify: {
-    0: 'Issues not fixed',
-    1: 'Not all required fields was filled',
+    0: 'Issues were not fixed',
+    1: 'Not all required fields were filled',
     2: 'Show',
     3: 'Not found',
     4: 'Banner not found (Currently, the first screenshot is being used as the banner)',
@@ -339,7 +339,7 @@ export default {
     7: 'ID not found',
     8: 'ID of the video not found, try another link (At the moment only YouTube is supported)',
     9: 'Oops',
-    10: 'Looks like your session is expired, open new page and log into your account (Don\'t worry, everything is saved automatically)',
+    10: 'Looks like your session expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
     11: 'Exists',
     12: 'This video has already been added',
     13: 'Exists',
@@ -354,33 +354,33 @@ export default {
     22: 'New creator has been added',
     23: 'HUH',
     24: 'Impossible to remove a deleted creator',
-    25: 'Exists',
-    26: 'Same change with this type has been added earlier',
-    27: 'Warning',
-    28: 'Same url with this type has been added earlier',
+    25: 'Duplicate',
+    26: 'This change has already been added earlier.',
+    27: 'Duplicate',
+    28: 'This url has already been added earlier.',
     29: 'Warning',
-    30: 'You forgot to add a description for a url. Example: Description:url',
+    30: 'You forgot to add a description for the url. Example: Description:url',
     31: 'Warning',
-    32: 'You forgot to add a url for description. Example: Description:url',
+    32: 'You forgot to add an url for the description. Example: Description:url',
     33: 'Restored',
-    34: 'Version {name} has been restored',
+    34: 'Version «{name}» has been restored',
     35: 'Deleted',
-    36: 'Version {name} has been deleted',
+    36: 'Version «{name}» has been deleted',
     37: 'Error',
-    38: 'Seems like the server thinks you\'re a bot',
-    39: 'Looks like you\'re user session is expired, open new page and log into your account (Don\'t worry, everything is saved automatically)',
-    40: 'Mismatched credentials (re:login in account. Don\'t worry, everything is saved automatically)',
+    38: 'Apparently the server thinks you\'re a robot.',
+    39: 'Looks like your user session has expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
+    40: 'Mismatched credentials (relogin in account. Don\'t worry, everything was saved automatically)',
     41: 'Unknown error',
-    42: 'You cannot edit inexistent skin',
-    43: 'Seems like you don\'t have permissions to edit this skin',
-    44: 'You cannot submit this version as it is the same as the previous one',
-    45: 'Those changes has been sent already on moderation',
+    42: 'You cannot edit a nonexistent skin',
+    43: 'Seems like you don\'t have permission to edit this skin',
+    44: 'You cannot send this version because it is the same as the previous one.',
+    45: 'These changes have already been sent for moderation',
     46: 'Success',
     47: 'New changes have been submitted for moderation',
     48: 'Changes have been updated in moderation',
     49: 'Odd link',
     50: 'This link cannot be used as a download link',
-    51: 'Submitted to moderation'
+    51: 'Submitted for moderation'
   },
   restricted_view: {
     0: 'Ikke autorisert',
