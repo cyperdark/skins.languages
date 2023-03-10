6efda51f7e052994174bce87e758820bfaa718a7
cyperdark <aburachenko2000@gmail.com>
2023-03-10 09:41:50 +0300

    New translations en.js (Chinese Simplified)

--- zhcn.js
+++ zhcn.js
@@ -6,7 +6,7 @@ export default {
     tags: {
       0: '标签',
       1: '{amount} 个标签属于 {name} 类别',
-      2: 'Search tags'
+      2: '搜索标签'
     },
     ideas: {
       0: '灵感',
@@ -25,7 +25,7 @@ export default {
     0: '更新日志',
     1: '当前版本完成的所有修订列表',
     2: '这里什么也没有',
-    3: 'But you can add the changes you\'ve made to this version of the skin',
+    3: '但您可以添加对此版本的皮肤所作的更改',
     4: '未分组',
     5: '新增',
     6: '已修复',
@@ -34,7 +34,7 @@ export default {
   },
   ideas: {
     0: '未找到任何内容',
-    1: 'Screenshots of «{type}» were not uploaded to the site'
+    1: '«{type}» 的屏幕截图尚未上传'
   },
   creators: {
     0: '作者',
@@ -50,8 +50,8 @@ export default {
     1: '与此皮肤相关的所有标签列表',
     2: '这里似乎什么也没有',
     3: '使用上面的字段查找新标签',
-    4: 'Tags not found',
-    5: 'Try searching for something else',
+    4: '未找到标签',
+    5: '请尝试搜索其他内容',
     6: '搜索'
   },
   videos: {
@@ -71,7 +71,7 @@ export default {
     0: '全部',
     1: '元数据',
     2: '高光',
-    3: 'Name of the skin not specified',
+    3: '未指定皮肤名称',
     4: '添加至少一个皮肤作者',
     5: '添加多个标签',
     6: '皮肤大小未指定',
@@ -81,10 +81,10 @@ export default {
     10: '未提供截图',
     11: '添加至少一张游戏截图',
     12: '皮肤链接未指定',
-    20: 'Looks pretty good',
-    21: 'Every issue was fixed. Good job!',
+    20: '看起来很不错',
+    21: '所有问题都已解决。干得好！',
     22: '检查清单',
-    23: 'Add at least one public version'
+    23: '添加至少一个公开版本'
   },
   skins: {
     0: '皮肤',
@@ -100,7 +100,7 @@ export default {
     10: '{name} 制作的 osu 皮肤 | 所有来自... 皮肤',
     11: '搜索',
     12: '{name} skins',
-    13: 'Explore skins'
+    13: '探索皮肤'
   },
   skin: {
     0: '编辑',
@@ -130,9 +130,9 @@ export default {
     25: '您不是我们的成员',
     26: '看起来您不是此皮肤的作者或编辑者',
     27: '皮肤由',
-    28: 'No tags',
-    29: 'No videos',
-    30: 'No creators'
+    28: '没有标签',
+    29: '没有视频',
+    30: '没有创作者'
   },
   drag: {
     0: '点击这里',
@@ -151,7 +151,7 @@ export default {
     5: '所有旗帜图像都来自 {name}',
     6: '本地化',
     7: '有人吗？[{lang}]',
-    8: 'Seems like cyperdark forgot to add them. {text}'
+    8: '似乎 cyperdark 忘记添加了。{text}'
   },
   search: {
     0: '皮肤查询',
@@ -161,15 +161,15 @@ export default {
     4: '尺寸',
     5: '日期',
     7: '未找到任何内容',
-    8: 'Try using different search filters',
+    8: '尝试使用其他搜索过滤规则',
     9: '待查询中',
-    10: 'Use the search filters or type the skin\'s title, author or tags',
+    10: '使用搜索过滤规则或者输入皮肤的标题、作者或标签',
     11: '下载',
     12: '名称',
     13: {
       0: '皮肤',
       1: '标签',
-      2: 'Users',
+      2: '用户',
       3: '列表'
     }
   },
@@ -183,8 +183,8 @@ export default {
     1: 'Unfortunately, there\'s nothing here, but you can take a look at the other pages',
     2: 'Unfortunately, there\'s nothing here',
     3: '无权限',
-    4: 'You need to be logged in order to edit this skin.',
-    5: 'You need to be logged in order to add a new skin.'
+    4: '您需要登录才能编辑此皮肤。',
+    5: '您需要登录才能添加新皮肤。'
   },
   meta: {
     title: {
@@ -199,7 +199,7 @@ export default {
       8: '{name} 搜索 osu 皮肤',
       9: '从其他 osu 皮肤中获得灵感',
       10: '{name} 制作的 osu 皮肤',
-      11: 'Search tags',
+      11: '搜索标签',
       12: '{name} found tags'
     },
     description: {
@@ -213,7 +213,7 @@ export default {
       7: '大量{name} osu皮肤，免费下载！',
       8: '{name} 制作的所有 osu! 皮肤',
       9: '在我们的网站上为您的下一个 osu! 皮肤寻找灵感。浏览多样化的皮肤集合，发现新技巧和想法。受到启发并创建自己独特的皮肤。',
-      10: 'Search for tags among all the tags on the site.'
+      10: '在所有标签中搜索。'
     }
   },
   buttons: {
@@ -247,7 +247,7 @@ export default {
     3: '喜欢: {likes} / 不喜欢: {dislikes}',
     4: '按住 ctrl+回车键 添加标签',
     5: '编辑版本',
-    6: '{s}Log in{p}, in order to rate this skin',
+    6: '{s}登录{p}，来给这个皮肤评分',
     7: '没有简短描述',
     8: '没有网址',
     9: '下载次数: {amount}'
@@ -308,84 +308,84 @@ export default {
     9: '游戏模式',
     10: '选择类型',
     11: '粘贴一个 YouTube 链接',
-    12: 'Type a description and an url. Format description:url',
+    12: '输入描述和 URL。格式：“描述:URL”',
     13: '选择额外的',
     14: '粘贴链接',
-    15: 'Skin title, author, or tags',
-    16: 'Select category',
-    17: 'Type the name of the tag'
+    15: '皮肤标题、作者或标签',
+    16: '选择类别',
+    17: '输入标签名称'
   },
   contribute: { 0: '贡献' },
   nsfw: {
     0: '警告！',
     1: '此皮肤包含敏感或成人内容',
     2: '此皮肤含有敏感或成人内容，某些用户可能发现令人不安的',
-    3: 'Alright',
-    4: 'bye o/'
+    3: '好吧',
+    4: '拜拜 o/'
   },
   removed: {
-    0: 'Version unavailable',
-    1: 'The author of the skin decided to remove this version from the site.',
-    2: 'Switch to an available version'
+    0: '版本不可用',
+    1: '皮肤作者决定将此版本删除。',
+    2: '切换到可用版本'
   },
   notify: {
-    0: 'Issues were not fixed',
-    1: 'Not all required fields were filled',
-    2: 'Show',
-    3: 'Not found',
-    4: 'Banner not found (Currently, the first screenshot is being used as the banner)',
-    5: 'No changes',
-    6: 'No changes were found',
-    7: 'ID not found',
-    8: 'ID of the video not found, try another link (At the moment only YouTube is supported)',
-    9: 'Oops',
-    10: 'Looks like your session expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
+    0: '问题未解决',
+    1: '还有必填字段尚未填写',
+    2: '显示',
+    3: '未找到',
+    4: '未找到横幅（目前横幅展示第一个截图）',
+    5: '没有更改',
+    6: '未找到更改',
+    7: '未找到 ID',
+    8: '未找到视频的 ID，请尝试另一个链接（目前仅支持 YouTube）',
+    9: '糟糕',
+    10: '您的会话似乎已过期，请打开一个新页面并登录您的账户（不必担心，所有内容都已自动保存）',
     11: 'Exists',
-    12: 'This video has already been added',
+    12: '此视频已添加',
     13: 'Exists',
-    14: 'This tag has already been added',
-    15: 'Not found',
-    16: 'You cannot delete a deleted tag',
-    17: 'Added',
-    18: 'You have successfully added a new tag',
-    19: 'OOps',
-    20: 'This creator was already added',
+    14: '此标签已添加',
+    15: '未找到',
+    16: '您不能删除一个已被删除的标签',
+    17: '已添加',
+    18: '已成功添加新标签',
+    19: '糟糕',
+    20: '已添加此创作者',
     21: 'EZ',
-    22: 'New creator has been added',
+    22: '已添加新的创作者',
     23: 'HUH',
-    24: 'Impossible to remove a deleted creator',
+    24: '无法删除一个已被删除的创作者',
     25: 'Duplicate',
-    26: 'This change has already been added earlier.',
+    26: '此更改已在之前添加。',
     27: 'Duplicate',
-    28: 'This url has already been added earlier.',
-    29: 'Warning',
-    30: 'You forgot to add a description for the url. Example: Description:url',
-    31: 'Warning',
-    32: 'You forgot to add an url for the description. Example: Description:url',
-    33: 'Restored',
-    34: 'Version «{name}» has been restored',
-    35: 'Deleted',
-    36: 'Version «{name}» has been deleted',
-    37: 'Error',
-    38: 'Apparently the server thinks you\'re a robot.',
-    39: 'Looks like your user session has expired, open a new page and log into your account (Don\'t worry, everything was saved automatically)',
-    40: 'Mismatched credentials (relogin in account. Don\'t worry, everything was saved automatically)',
-    41: 'Unknown error',
-    42: 'You cannot edit a nonexistent skin',
-    43: 'Seems like you don\'t have permission to edit this skin',
-    44: 'You cannot send this version because it is the same as the previous one.',
-    45: 'These changes have already been sent for moderation',
-    46: 'Success',
-    47: 'New changes have been submitted for moderation',
-    48: 'Changes have been updated in moderation',
+    28: '此 URL 已在之前添加。',
+    29: '注意',
+    30: '您尚未为 URL 添加描述。示例：“描述:URL”',
+    31: '注意',
+    32: '您尚未为描述添加 URL。示例：“描述:URL”',
+    33: '已恢复',
+    34: '版本 «{name}» 已恢复',
+    35: '已删除',
+    36: '版本 «{name}» 已删除',
+    37: '错误',
+    38: '显然服务器把您当成了机器人。',
+    39: '您的用户会话似乎已过期，请打开一个新页面并登录您的账户（不必担心，所有内容都已自动保存）',
+    40: '凭据不匹配（请重新登录账户。不必担心，所有内容都已自动保存）',
+    41: '未知错误',
+    42: '您不能编辑一个不存在的皮肤',
+    43: '您似乎没有权限编辑此皮肤',
+    44: '您不能发送此版本，因为它与上一个版本相同。',
+    45: '这些更改已经发送以供审核',
+    46: '成功',
+    47: '新的更改已提交审核',
+    48: '更改已更新至审核',
     49: 'Odd link',
-    50: 'This link cannot be used as a download link',
-    51: 'Submitted for moderation'
+    50: '此链接无法用作下载链接',
+    51: '已提交审核'
   },
   restricted_view: {
     0: '无权限',
-    1: 'Please log into your account',
-    2: 'Restricted view',
+    1: '请登录您的账户',
+    2: '受限视图',
     3: 'This page is not out, yet.'
   }
 };
