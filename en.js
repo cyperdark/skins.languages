f2d2a523303c0a41d7349a12ff5efc2afd3a351b
cyperdark <aburachenko2000@gmail.com>
2023-04-23 14:56:11 +0300

    Update en.js

--- en.js
+++ en.js
@@ -189,7 +189,9 @@ export default {
       1: 'Tags',
       2: 'Users',
       3: 'Lists',
-    }
+    },
+
+    14: 'Search:',
   },
   tabs: {
     0: 'Search',
@@ -219,6 +221,7 @@ export default {
       10: '{name} created osu skins',
       11: 'Search tags',
       12: '{name} found tags',
+      13: 'New skin',
     },
     description: {
       0: 'Get the best osu! skins at our site! Our huge library has skins for all gamemodes, aspect ratios and even top players. Plus, we have a special section for anime fans. Improve your gameplay with the perfect skin. #osuskins #osugame #anime',
@@ -425,6 +428,11 @@ export default {
     50: 'This link cannot be used as a download link',
 
     51: 'Submitted for moderation',
+  },
+
+  header: {
+    0: 'Notifications',
+    1: 'Follow us on these platforms to receive notifications about new skins',
   },
 
   restricted_view: {
