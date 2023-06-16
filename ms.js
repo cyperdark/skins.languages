7595f3f1e2a978be4a5aea7686f398c345711eb9
cyperdark <aburachenko2000@gmail.com>
2023-06-16 23:09:01 +0300

    New translations en.js (Malay)

--- ms.js
+++ ms.js
@@ -147,7 +147,9 @@ export default {
     32: 'Undi naik & komen di reddit',
     33: 'Komen di osu! forum',
     34: 'Skin lain daripada pengarang ini | Skin lain daripada pengarang ini',
-    35: 'Sokong skin ini'
+    35: 'Sokong skin ini',
+    36: 'Unavailable',
+    37: 'Unfortunately skin was deleted by {author} request'
   },
   drag: {
     0: 'Ketik di sini',
@@ -334,11 +336,12 @@ export default {
   },
   contribute: { 0: 'Sumbangan' },
   nsfw: {
-    0: 'Amaran!',
+    0: 'Warning nsfw!',
     1: 'Skin ini mengandungi kandungan sensitif ataupun kandungan dewasa',
     2: 'Skin ini mengandungi kandungan sensitif atau kandungan dewasa yang mungkin tidak disukai oleh sesetengah pengguna',
     3: 'Ok',
-    4: 'jumpa lagi o/'
+    4: 'jumpa lagi o/',
+    5: 'Warning furry!'
   },
   removed: {
     0: 'Versi tidak ada',
