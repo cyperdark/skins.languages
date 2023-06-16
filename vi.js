a014c3a9d8c871e10aac9730953ffa8f3a1d70e3
cyperdark <aburachenko2000@gmail.com>
2023-06-16 23:08:57 +0300

    New translations en.js (Vietnamese)

--- vi.js
+++ vi.js
@@ -147,7 +147,9 @@ export default {
     32: 'Upvote & comment on reddit',
     33: 'Comment on osu! forum',
     34: 'More skins from this author | More skins from this authors',
-    35: 'Support the skin'
+    35: 'Support the skin',
+    36: 'Unavailable',
+    37: 'Unfortunately skin was deleted by {author} request'
   },
   drag: {
     0: 'Nhấn vào đây',
@@ -334,11 +336,12 @@ export default {
   },
   contribute: { 0: 'Đóng góp' },
   nsfw: {
-    0: 'Cảnh báo!',
+    0: 'Warning nsfw!',
     1: 'Skin có chứa các nội dung nhạy cảm hoặc người lớn',
     2: 'Skin có chứa các nội dung nhạy cảm có thể gây khó chịu cho một số người dùng',
     3: 'Được',
-    4: 'tạm biệt o/'
+    4: 'tạm biệt o/',
+    5: 'Warning furry!'
   },
   removed: {
     0: 'Phiên bản không khả dụng',
