fb89e0d5d6f0eb352925336519f1ef82243e4bd1
cyperdark <aburachenko2000@gmail.com>
2023-06-16 23:08:51 +0300

    New translations en.js (Serbian (Cyrillic))

--- sr.js
+++ sr.js
@@ -147,7 +147,9 @@ export default {
     32: 'Upvote & comment on reddit',
     33: 'Comment on osu! forum',
     34: 'More skins from this author | More skins from this authors',
-    35: 'Support the skin'
+    35: 'Support the skin',
+    36: 'Unavailable',
+    37: 'Unfortunately skin was deleted by {author} request'
   },
   drag: {
     0: 'Click here',
@@ -334,11 +336,12 @@ export default {
   },
   contribute: { 0: 'Contribute' },
   nsfw: {
-    0: 'Упозорење!',
+    0: 'Warning nsfw!',
     1: 'This skin contains sensitive or adult content',
     2: 'This skin contains sensitive or adult content that some users may find disturbing',
     3: 'У реду',
-    4: 'bye o/'
+    4: 'bye o/',
+    5: 'Warning furry!'
   },
   removed: {
     0: 'Version unavailable',
