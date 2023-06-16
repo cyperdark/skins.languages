1f070b50f4272a01865a37e832153aa28760ec13
cyperdark <aburachenko2000@gmail.com>
2023-06-16 23:08:56 +0300

    New translations en.js (Chinese Traditional)

--- zhtw.js
+++ zhtw.js
@@ -147,7 +147,9 @@ export default {
     32: 'Upvote & comment on reddit',
     33: 'Comment on osu! forum',
     34: 'More skins from this author | More skins from this authors',
-    35: 'Support the skin'
+    35: 'Support the skin',
+    36: 'Unavailable',
+    37: 'Unfortunately skin was deleted by {author} request'
   },
   drag: {
     0: '點擊此處',
@@ -334,11 +336,12 @@ export default {
   },
   contribute: { 0: '貢獻' },
   nsfw: {
-    0: '警告!',
+    0: 'Warning nsfw!',
     1: '這個skin包含敏感或成人內容',
     2: '此skin包含一些可能會讓人感到不安的敏感或成人內容',
     3: 'Alright',
-    4: 'bye o/'
+    4: 'bye o/',
+    5: 'Warning furry!'
   },
   removed: {
     0: 'Version unavailable',
