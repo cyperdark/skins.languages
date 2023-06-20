ae430a3a4ef5fc88aa831450fcd57bc77f07539c
cyperdark <aburachenko2000@gmail.com>
2023-06-20 23:08:39 +0300

    New translations en.js (Finnish)

--- fi.js
+++ fi.js
@@ -149,7 +149,7 @@ export default {
     34: 'More skins from this author | More skins from this authors',
     35: 'Support the skin',
     36: 'Unavailable',
-    37: 'Unfortunately skin was deleted by {author} request'
+    37: 'Unfortunately, this skin was deleted at {author} request'
   },
   drag: {
     0: 'Paina tästä',
@@ -336,12 +336,12 @@ export default {
   },
   contribute: { 0: 'Osallistu' },
   nsfw: {
-    0: 'Warning nsfw!',
+    0: 'Warning, nsfw!',
     1: 'Tämä skini sisältää herkkää tai aikuisille tarkoitettua sisältöä',
     2: 'Tämä skini sisältää herkkää tai aikuisille tarkoitettua sisältöä, jota jotkut käyttäjät voivat pitää häiritsevänä',
     3: 'Selvä',
     4: 'Heippa o/',
-    5: 'Warning furry!'
+    5: 'Furry Warning!'
   },
   removed: {
     0: 'Versio ei ole saatavilla',
