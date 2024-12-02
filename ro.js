6135f97f3824eb4cab661d1fa36d21761191cde9
cyperdark <21735205+cyperdark@users.noreply.github.com>
2024-12-02 08:13:19 +0300

    New translations en.js (Romanian)

--- ro.js
+++ ro.js
@@ -97,7 +97,6 @@ export default {
     24: 'Adaugă cel puțin un creator la această versiune',
     25: 'Unii creatori nu au roluri',
     26: '{amount} screenshots doesn\'t have category',
-    27: 'Specifiy gameplay name for {mode} screenshots | Specifiy gameplay name for {mode} screenshot',
     28: 'Select gamemode for gameplay screenshots'
   },
   skins: {
