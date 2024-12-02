8af44e74bc1d90d390752482629e7df811064311
cyperdark <21735205+cyperdark@users.noreply.github.com>
2024-12-02 15:47:56 +0300

    New translations en.js (Slovak)

--- sk.js
+++ sk.js
@@ -95,7 +95,7 @@ export default {
     22: 'Kontrolná listina',
     23: 'Pridajte aspoň jednu publikovanú verziu',
     24: 'Add at least one creator to this version',
-    25: 'Some creators doesn\'t have roles',
+    25: 'Some creators don\'t have roles',
     26: '{amount} screenshots doesn\'t have category',
     28: 'Select gamemode for gameplay screenshots'
   },
