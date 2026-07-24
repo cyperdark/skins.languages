db8d1c1637ff5c3cc80df24141e9ec3455d26a84
cyperdark <21735205+cyperdark@users.noreply.github.com>
2026-07-24 10:20:38 +0300

    New translations en.js (Indonesian)
    [ci skip]

--- id.js
+++ id.js
@@ -400,6 +400,7 @@ export default {
     85: 'try disabling «Quick select»',
     86: 'Submit again',
     87: 'Use this one if you have multiple gameplay variants that appear visually different',
+    88: 'Clear changes?',
     checks: {
       0: 'Nama skin belum diisi',
       1: 'Tambahkan setidaknya satu pembuat',
