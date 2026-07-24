3360a4967eeb3e7a26443779bd49efa4e3236ed1
cyperdark <21735205+cyperdark@users.noreply.github.com>
2026-07-24 10:20:07 +0300

    New translations en.js (French)
    [ci skip]

--- fr.js
+++ fr.js
@@ -400,6 +400,7 @@ export default {
     85: 'try disabling «Quick select»',
     86: 'Submit again',
     87: 'Use this one if you have multiple gameplay variants that appear visually different',
+    88: 'Clear changes?',
     checks: {
       0: 'Nom du skin non spécifié',
       1: 'Ajoutez au moins un créateur',
