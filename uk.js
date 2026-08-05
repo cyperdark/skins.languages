04ef9b2ab0ba83adbbb9347ad9d106d19f14770d
cyperdark <21735205+cyperdark@users.noreply.github.com>
2026-08-05 21:40:33 +0300

    New translations en.js (Ukrainian)
    [ci skip]

--- uk.js
+++ uk.js
@@ -402,6 +402,11 @@ export default {
     86: 'Submit again',
     87: 'Use this one if you have multiple gameplay variants that appear visually different',
     88: 'Clear changes?',
+    89: 'osu! forum',
+    90: 'reddit',
+    91: 'twitter',
+    92: 'Newer version available: {version}',
+    93: '{amount} new version available | {amount} new versions available',
     checks: {
       0: 'Назву скіна не вказано',
       1: 'Додайте хоча б одного автора',
